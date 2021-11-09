71beb6380b53353930427ad029b471a57925e19a
SageanneS <sageanne.senneff@ucdconnect.ie>
2021-11-09 14:54:50 -0500

    updated circuit model and reduced maxiters

--- test/fitting.jl
+++ test/fitting.jl
@@ -1,38 +1,47 @@
 using Neuroblox, LinearAlgebra, OrdinaryDiffEq, GalacticOptim, Optim, ForwardDiff, Test
 
 # Create Regions
-@named GPe = NeuralMass(activation="a_tan", ω=4*2*π, ζ=1, k=(4*2*π)^2, h=1)
-@named STN = NeuralMass(activation="a_tan", ω=4*2*π, ζ=1, k=(4*2*π)^2, h=10)
+@named Str = NeuralMass(activation="logistic", τ=0.0022, H=20, λ=300, r=0.3)
+@named GPe = NeuralMass(activation="logistic", τ=0.04, H=20, λ=400, r=0.1)
+@named STN = NeuralMass(activation="logistic", τ=0.01, H=20, λ=500, r=0.1)
 
 # Connect Regions
-sys = [GPe, STN]
-params =  @parameters g_GPe_STN=1.0 g_STN_GPe=1.0
-# inhibitory signs must go directly into adj_matrix to run fitting algorithm
-adj_matrix = [sys[1].x g_STN_GPe*sys[2].x; 
-              -g_GPe_STN*sys[1].x sys[2].x]
+sys = [Str, GPe, STN]
+@parameters C_Cor=60 C_BG_Th=60 C_Cor_BG_Th=5 C_BG_Th_Cor=5
+adj_matrix = [0 0 0;
+             -0.5*C_BG_Th*sys[1].x -0.5*C_BG_Th*sys[2].x C_BG_Th*sys[3].x;
+              0 -0.5*C_BG_Th*sys[2].x 0];
 @named BG_Circuit = Connections(sys=sys, adj_matrix=adj_matrix)
 
-sim_dur = 10.0 # Simulate for 10 Seconds
+sim_dur = 5.0 # Simulation time (seconds)
 prob = ODAEProblem(structural_simplify(BG_Circuit), [], (0.0, sim_dur), [])
 
+# Fitting Procedure
+# Grab system parameters
 ptrue = prob.p
+# Alter system parameters randomly
 pinit = ptrue .+ ptrue .* randn(length(prob.p)) ./ 100
-data = solve(prob, Vern9(), abstol=1e-12, reltol=1e-12, saveat=0.1)
+# Solve problem as normal
+data = solve(prob, Vern9(), abstol=1e-12, reltol=1e-12, saveat=0.001)
+
+# Define loss function
 lb = 0.1 .* ptrue
 ub = 2 .* ptrue
 internalnorm(u,t) = sqrt(ForwardDiff.value(sum(abs2,u)))/length(u)
-
 function loss(p,_)
-    sol = solve(remake(prob,p=p), Tsit5(), abstol=1e-12, reltol=1e-12, saveat=0.1)
+    sol = solve(remake(prob,p=p), Tsit5(), abstol=1e-12, reltol=1e-12, saveat=0.001)
     if sol.retcode != :Success
-        Inf * p[1]
+        Inf * abs(p[1])
     else
-        sum(abs2, sol .- data)
+        sum(abs2, sol .- data) 
     end
 end
 optfun = OptimizationFunction(loss,GalacticOptim.AutoForwardDiff())
 optprob = OptimizationProblem(optfun,pinit,lb=lb,ub=ub)
-optsol = solve(optprob,GradientDescent(),maxiters = 10000)
 
-@test_broken !iszero(norm(optsol.u - pinit))
-#@test norm(optsol.u - ptrue) < 1
+# Solve optimization problem
+optsol = solve(optprob,GradientDescent(),maxiters = 3000)
+
+# Compare results (test)
+#@test_broken !iszero(norm(optsol.u - pinit))
+@test norm(optsol.u - ptrue) < 1
