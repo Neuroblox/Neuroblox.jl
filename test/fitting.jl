984b741b98f5f41f9c8c1be437bdbcd4b15ab8b5
SageanneS <sageanne.senneff@ucdconnect.ie>
2021-11-17 10:44:37 -0500

    trying multiple circuits increasing complexity

--- test/fitting.jl
+++ test/fitting.jl
@@ -1,14 +1,41 @@
 using Neuroblox, LinearAlgebra, OrdinaryDiffEq, GalacticOptim, Optim, ForwardDiff, Test
 
+## This model works:
 # Create Regions
-@named GPe = NeuralMass(activation="a_tan", ω=4*2*π, ζ=1, k=(4*2*π)^2, h=0.1)
-@named STN = NeuralMass(activation="a_tan", ω=4*2*π, ζ=1, k=(4*2*π)^2, h=1)
+#@named GPe = NeuralMass(activation="a_tan", ω=4*2*π, ζ=1, k=(4*2*π)^2, h=0.1)
+#@named STN = NeuralMass(activation="a_tan", ω=4*2*π, ζ=1, k=(4*2*π)^2, h=1)
+# Connect Regions through Adjacency Matrix
+#sys = [GPe, STN]
+#@parameters g_GPe_STN=1.0 g_STN_GPe=1.0
+#adj_matrix = [sys[1].x g_STN_GPe*sys[2].x; 
+#             -g_GPe_STN*sys[1].x sys[2].x]
+
+## Try a more complicated circuit: It works 
+#@named GPe = NeuralMass(activation="logistic", τ=0.04, H=20, λ=400, r=0.1)
+#@named STN = NeuralMass(activation="logistic", τ=0.01, H=20, λ=500, r=0.1)
+#@named GPi = NeuralMass(activation="logistic", τ=0.014, H=20, λ=400, r=0.1)
 
 # Connect Regions through Adjacency Matrix
-sys = [GPe, STN]
-@parameters g_GPe_STN=1.0 g_STN_GPe=1.0
-adj_matrix = [sys[1].x g_STN_GPe*sys[2].x; 
-             -g_GPe_STN*sys[1].x sys[2].x]
+#sys = [GPe, STN, GPi]
+#@parameters C_Cor=3 C_BG_Th=3 C_Cor_BG_Th=9.75 C_BG_Th_Cor=9.75
+#adj_matrix = [-0.5*C_BG_Th*sys[1].x C_BG_Th*sys[2].x 0;
+#              -0.5*C_BG_Th*sys[1].x 0 0
+#              -0.5*C_BG_Th*sys[1].x C_BG_Th*sys[2].x 0]
+
+# Try an even more complicated circuit: It works!
+@named Str = NeuralMass(activation="logistic", τ=0.0022, H=20, λ=300, r=0.3)
+@named GPe = NeuralMass(activation="logistic", τ=0.04, H=20, λ=400, r=0.1)
+@named STN = NeuralMass(activation="logistic", τ=0.01, H=20, λ=500, r=0.1)
+@named GPi = NeuralMass(activation="logistic", τ=0.014, H=20, λ=400, r=0.1)
+
+# Connect Regions through Adjacency Matrix
+sys = [Str, GPe, STN, GPi]
+@parameters C_Cor=3 C_BG_Th=3 C_Cor_BG_Th=9.75 C_BG_Th_Cor=9.75
+adj_matrix = [0 0 0 0;
+             -0.5*C_BG_Th*sys[1].x -0.5*C_BG_Th*sys[2].x C_BG_Th*sys[3].x 0;
+              0 -0.5*C_BG_Th*sys[2].x 0 0;
+              0 -0.5*C_BG_Th*sys[2].x C_BG_Th*sys[3].x 0]
+
 @named BG_Circuit = Connections(sys=sys, adj_matrix=adj_matrix)
 
 sim_dur = 5.0 # Simulation time (seconds)
@@ -38,8 +65,8 @@ optfun = OptimizationFunction(loss,GalacticOptim.AutoForwardDiff())
 optprob = OptimizationProblem(optfun,pinit,lb=lb,ub=ub)
 
 # Solve optimization problem
-optsol = solve(optprob, BFGS(),maxiters = 200)
+optsol = solve(optprob, BFGS(),maxiters = 600)
 
 # Calculate squared error of fit
-@test norm(optsol.u - ptrue) < 1 # balance threshold value and maxiters for run time
+@test norm(optsol.u - ptrue) < 16 # balance threshold value and maxiters for run time
 #@test_broken !iszero(norm(optsol.u - pinit))
