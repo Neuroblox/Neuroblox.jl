2fda5c0b1798ccdad1747a361fcda4f497f41c57
Christopher Rackauckas <accounts@chrisrackauckas.com>
2021-12-22 05:44:23 -0500

    Update src/Neurographs.jl

--- src/Neurographs.jl
+++ src/Neurographs.jl
@@ -4,9 +4,9 @@ struct LinearNeuroGraph <: AbstractNeuroGraph; graph::MetaDiGraph end
 struct OtherNeuroGraph <: AbstractNeuroGraph; graph::MetaDiGraph end
 
 # method forward does not work
-@inline Graphs.add_edge!(g::AbstractNeuroGraph, x...) = add_edge!(g.graph, x...)
-@inline Graphs.add_vertex!(g::AbstractNeuroGraph, x...) = add_vertex!(g.graph, x...)
-@inline Graphs.rem_vertex!(g::AbstractNeuroGraph, x...) = rem_vertex!(g.graph, x...)
+Graphs.add_edge!(g::AbstractNeuroGraph, x...) = add_edge!(g.graph, x...)
+Graphs.add_vertex!(g::AbstractNeuroGraph, x...) = add_vertex!(g.graph, x...)
+Graphs.rem_vertex!(g::AbstractNeuroGraph, x...) = rem_vertex!(g.graph, x...)
 
 function AdjMatrixfromLinearNeuroGraph(g::LinearNeuroGraph)
     myadj = map(Float64,adjacency_matrix(g.graph))
