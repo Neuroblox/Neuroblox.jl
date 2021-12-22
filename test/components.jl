c2b99aad75a45c02896620e7c3c285d7ccc76401
Chris Rackauckas <accounts@chrisrackauckas.com>
2021-12-22 00:13:12 -0500

    something fits

--- test/components.jl
+++ test/components.jl
@@ -15,13 +15,13 @@ sys = [Str, GPe, STN, GPi, Th, EI, PY, II]
 
 @parameters C_Cor=60 C_BG_Th=60 C_Cor_BG_Th=5 C_BG_Th_Cor=5
 adj_matrix = [0 0 0 0 0 0 0 0;
--0.5*C_BG_Th*sys[1].x -0.5*C_BG_Th*sys[2].x C_BG_Th*sys[3].x 0 0 0 0 0;
-              0 -0.5*C_BG_Th*sys[2].x 0 0 0 0 C_Cor_BG_Th*sys[7].x 0;
-              0 -0.5*C_BG_Th*sys[2].x C_BG_Th*sys[3].x 0 0 0 0 0;
-              0 0 0 -0.5*C_BG_Th*sys[4].x 0 0 0 0;
-              0 0 0 0 C_BG_Th_Cor*sys[5].x 0 6*C_Cor*sys[7].x 0;
-              0 0 0 0 0 4.8*C_Cor*sys[6].x 0 -1.5*C_Cor*sys[8].x;
-              0 0 0 0 0 0 1.5*C_Cor*sys[7].x 3.3*C_Cor*sys[8].x]
+            -0.5*C_BG_Th*sys[1].x -0.5*C_BG_Th*sys[2].x C_BG_Th*sys[3].x 0 0 0 0 0;
+            0 -0.5*C_BG_Th*sys[2].x 0 0 0 0 C_Cor_BG_Th*sys[7].x 0;
+            0 -0.5*C_BG_Th*sys[2].x C_BG_Th*sys[3].x 0 0 0 0 0;
+            0 0 0 -0.5*C_BG_Th*sys[4].x 0 0 0 0;
+            0 0 0 0 C_BG_Th_Cor*sys[5].x 0 6*C_Cor*sys[7].x 0;
+            0 0 0 0 0 4.8*C_Cor*sys[6].x 0 -1.5*C_Cor*sys[8].x;
+            0 0 0 0 0 0 1.5*C_Cor*sys[7].x 3.3*C_Cor*sys[8].x]
 
 
 @named CBGTC_Circuit = Connections(sys=sys, adj_matrix=adj_matrix)
