8563934d6519e63919a1f8648d7542234ee5537e
SageanneS <sageanne.senneff@ucdconnect.ie>
2022-03-25 07:56:35 -0400

    fix test name

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,6 @@ using SafeTestsets
 @time @safetestset "Neurograph Tests" begin include("graphs.jl") end
 @time @safetestset "ODE from Graph Tests" begin include("ode_from_graph.jl") end
 @time @safetestset "Neural Signal Measurement Models Tests" begin include("measurement_models.jl") end
-@time @safetestset "Spectral Utilities Tests" begin include("spectral_tools.jl") end
+@time @safetestset "Spectral Utilities Tests" begin include("spectraltools.jl") end
 @time @safetestset "Dynamic Causal Modeling Tests" begin include("DCM.jl") end
 @time @safetestset "ODE from Graph and simulate" begin include("graph_to_dataframe.jl") end
