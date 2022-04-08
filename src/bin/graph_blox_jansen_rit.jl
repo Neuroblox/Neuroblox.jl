58254da0fcdae94ed494dfcf6c012df98875ec66
Helmut Strey <Helmut.Strey@gmail.com>
2022-04-08 16:55:19 -0400

    created two examples of neuralmass circuits in the bin folder for Adrian.  One is just one blox and the other is a mixture

--- src/bin/graph_blox_jansen_rit.jl
+++ src/bin/graph_blox_jansen_rit.jl
@@ -2,34 +2,23 @@ using Neuroblox, Graphs, MetaGraphs
 
 @named Str = jansen_rit(τ=0.0022, H=20, λ=300, r=0.3)
 @named GPe = jansen_rit(τ=0.04, H=20, λ=400, r=0.1)
-@named STN = jansen_rit(τ=0.01, H=20, λ=500, r=0.1)
-@named GPi = jansen_rit(τ=0.014, H=20, λ=400, r=0.1)
+@named GPi = harmonic_oscillator(ω=4*2*π, ζ=1, k=(4*2*π)^2, h=0.1)
+@named STN = harmonic_oscillator(ω=4*2*π, ζ=1, k=(4*2*π)^2, h=1.0)
 
 # Connect Regions through Adjacency Matrix
-@parameters C_Cor=3 C_BG_Th=3 C_Cor_BG_Th=9.75 C_BG_Th_Cor=9.75
-
+@parameters C_Cor=2.0
 # Create Graph
 g = LinearNeuroGraph(MetaDiGraph())
 add_blox!(g,Str)
 add_blox!(g,GPe)
 add_blox!(g,STN)
 add_blox!(g,GPi)
-add_edge!(g,1,1,:weight,0.0)
-add_edge!(g,1,2,:weight,0.0)
-add_edge!(g,1,3,:weight,0.0)
-add_edge!(g,1,4,:weight,0.0)
-add_edge!(g,2,1,:weight,-0.5*C_BG_Th)
-add_edge!(g,2,2,:weight,-0.5*C_BG_Th)
-add_edge!(g,2,3,:weight,C_BG_Th)
-add_edge!(g,2,4,:weight,0.0)
-add_edge!(g,3,1,:weight,0.0)
-add_edge!(g,3,2,:weight,-0.5*C_BG_Th)
-add_edge!(g,3,3,:weight,0.0)
-add_edge!(g,3,4,:weight,0.0)
-add_edge!(g,4,1,:weight,0.0)
-add_edge!(g,4,2,:weight,-0.5*C_BG_Th)
-add_edge!(g,4,3,:weight, C_BG_Th)
-add_edge!(g,4,4,:weight,0.0)
+add_edge!(g,3,1,:weight,0.5*C_Cor)
+add_edge!(g,4,1,:weight,0.5*C_Cor)
+add_edge!(g,1,2,:weight,0.1*C_Cor)
+add_edge!(g,3,4,:weight,0.1*C_Cor)
+add_edge!(g,1,3,:weight,0.1*C_Cor)
+add_edge!(g,2,4,:weight,0.1*C_Cor)
 
 @named four_regions_gr = ODEfromGraph(g=g)
 
