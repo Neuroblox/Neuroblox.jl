de2f87a5441868c557e77ef3a2bf683e2c9d7e99
Helmut Strey <Helmut.Strey@gmail.com>
2022-04-25 19:56:23 -0400

    created eightregion example for demo

--- examples/eightregions.jl
+++ examples/eightregions.jl
@@ -18,9 +18,12 @@ end
 begin
     import Pkg
     # activate the shared project environment
-    Pkg.activate()
+    Pkg.activate(@__DIR__)
     # instantiate, i.e. make sure that all packages are downloaded
     Pkg.instantiate()
+	Pkg.add("OrdinaryDiffEq")
+	Pkg.add("MetaGraphs")
+	Pkg.add("Graphs")
 	Pkg.add("Plots")
 	Pkg.add(path=joinpath(@__DIR__, "..", "..", "Neuroblox.jl"))
 	Pkg.add("OrdinaryDiffEq")
@@ -30,14 +33,14 @@ end
 # ╔═╡ 66460e85-1544-406b-9c79-773ab174a5cb
 begin
 	# Create Regions
-	@named Str = jansen_ritC(τ=0.0022, H=20, λ=300, r=0.3)
-	@named GPe = jansen_ritC(τ=0.04, H=20, λ=400, r=0.1)
-	@named STN = jansen_ritC(τ=0.01, H=20, λ=500, r=0.1)
+	@named Str = jansen_ritSC(τ=0.0022, H=20, λ=300, r=0.3)
+	@named GPe = jansen_ritSC(τ=0.04, H=20, λ=400, r=0.1)
+	@named STN = jansen_ritSC(τ=0.01, H=20, λ=500, r=0.1)
 	@named GPi = jansen_ritSC(τ=0.014, H=20, λ=400, r=0.1)
 	@named Th  = jansen_ritSC(τ=0.002, H=10, λ=20, r=5)
-	@named EI  = jansen_ritSC(τ=0.01, H=20, λ=5, r=5)
-	@named PY  = jansen_ritSC(τ=0.001, H=20, λ=5, r=0.15)
-	@named II  = jansen_ritSC(τ=2.0, H=60, λ=5, r=5)
+	@named EI  = jansen_ritC(τ=0.01, H=20, λ=5, r=5)
+	@named PY  = jansen_ritC(τ=0.001, H=20, λ=5, r=0.15)
+	@named II  = jansen_ritC(τ=2.0, H=60, λ=5, r=5)
 end
 
 # ╔═╡ 9aa9ae2b-b8a0-463d-8e9e-b3339b25a99d
@@ -74,21 +77,11 @@ begin
 	add_edge!(g,7,8,:weight, -1.5*C_Cor)
 
 	add_edge!(g,8,7,:weight, 1.5*C_Cor)
-	add_edge!(g,8,8,:weight, 3.3*C_Cor)
+	add_edge!(g,8,8,:weight, -3.3*C_Cor)
 end
 
 # ╔═╡ 5fc63975-3a15-4430-a7f1-4e5db64c04a1
 AdjMatrixfromLinearNeuroGraph(g)
-
-# ╔═╡ 785d8e9a-9ec0-4a5d-92a4-1d1d18d4ffc0
-adj_matrix_lin = [0 0 0 0 0 0 0 0;
-            -0.5*C_BG_Th -0.5*C_BG_Th C_BG_Th 0 0 0 0 0;
-            0 -0.5*C_BG_Th 0 0 0 0 C_Cor_BG_Th 0;
-            0 -0.5*C_BG_Th C_BG_Th 0 0 0 0 0;
-            0 0 0 -0.5*C_BG_Th 0 0 0 0;
-            0 0 0 0 C_BG_Th_Cor 0 6*C_Cor 0;
-            0 0 0 0 0 4.8*C_Cor 0 -1.5*C_Cor;
-            0 0 0 0 0 0 1.5*C_Cor 3.3*C_Cor]
 
 # ╔═╡ 33002a2b-f8a9-4728-8288-2f92d3b89948
 @named eight_regions_gr = ODEfromGraph(g=g)
@@ -111,6 +104,21 @@ eight_regions_s.ps
 # ╔═╡ 0447ca7d-9dc0-4777-b4ac-adc2eb7d3c8a
 eight_regions_s.states
 
+# ╔═╡ a1c1b45f-8692-4456-ab20-d72b3e44fc0d
+indexof(sym,syms) = findfirst(isequal(sym),syms)
+
+# ╔═╡ f039dc58-04ba-4682-8f87-86ec19bd8d2f
+parameters(eight_regions_s)
+
+# ╔═╡ 413e1cd3-00cf-4bb3-98f2-35fa323454cd
+begin
+	# get the indices of the parameters in the parameter list
+	bgth_idx = indexof(C_BG_Th,parameters(eight_regions_s))
+	corbgth_idx = indexof(C_Cor_BG_Th,parameters(eight_regions_s))
+	cor_idx = indexof(C_Cor,parameters(eight_regions_s))
+	bgthcor_idx = indexof(C_BG_Th_Cor,parameters(eight_regions_s))
+end
+
 # ╔═╡ b94c0e24-1793-4fe0-b84d-974d0c27113c
 md"""
 BG-Th
@@ -126,12 +134,13 @@ $(@bind cor html"<input type=range min=0 max=10000>")
 
 # ╔═╡ 32d1b83e-acfa-4351-9bcf-bc4367781186
 begin
+	# set the parameters for the simulation using the sliders
 	p_new = prob.p
-	p_new[1] = (bgth)/100
-	p_new[2] = (corbgth-5000)/1000
-	p_new[3] = (cor)/100
-	p_new[4] = (bgthcor-5000)/1000
-	prob_new = remake(prob; p=p_new)
+	p_new[ bgth_idx ] = (bgth)/100
+	p_new[ corbgth_idx] = (corbgth-5000)/1000
+	p_new[cor_idx] = (cor)/100
+	p_new[bgthcor_idx] = (bgthcor-5000)/1000
+	prob_new = remake(prob; p=p_new, u0=ones(16)*0.1)
 	sol = solve(prob_new, Tsit5())
 end
 
@@ -140,14 +149,22 @@ end
 
 # ╔═╡ 8accf027-0261-42e5-ac11-c066cfb57c43
 begin
-	plot(sol.t,sol[1,:])
-	plot!(sol.t,sol[3,:])
-	plot!(sol.t,sol[5,:])
-	plot!(sol.t,sol[7,:])
-	plot!(sol.t,sol[9,:])
-	plot!(sol.t,sol[11,:])
-	plot!(sol.t,sol[13,:])
-	plot!(sol.t,sol[15,:])
+	l = @layout [a; b; c]
+	p1 = plot(sol.t,sol[11,:],label="EI")
+	p2 = plot(sol.t,sol[13,:],label="PY")
+	p3 = plot(sol.t,sol[15,:],label="II")
+	plot(p1, p2, p3, layout = l)
+end
+
+# ╔═╡ bcb92a18-166c-46a7-aace-ccca97a825e4
+begin
+	l2 = @layout [a b; c; d e]
+	p4 = plot(sol.t,sol[1,:],label="Str")
+	p5 = plot(sol.t,sol[3,:],label="GPe")
+	p6 = plot(sol.t,sol[5,:],label="STN")
+	p7 = plot(sol.t,sol[7,:],label="GPi")
+	p8 = plot(sol.t,sol[9,:],label="Th")
+	plot(p4, p5, p6, p7, p8, layout = l2)
 end
 
 # ╔═╡ Cell order:
@@ -156,7 +173,6 @@ end
 # ╠═9aa9ae2b-b8a0-463d-8e9e-b3339b25a99d
 # ╠═fef8036a-f073-4473-9549-2b69ec644e8a
 # ╠═5fc63975-3a15-4430-a7f1-4e5db64c04a1
-# ╠═785d8e9a-9ec0-4a5d-92a4-1d1d18d4ffc0
 # ╠═33002a2b-f8a9-4728-8288-2f92d3b89948
 # ╠═c4b4aa78-0324-4ea8-9903-efe87f6074e8
 # ╠═1a48d894-f43b-4559-8844-50b6e1989bda
@@ -164,7 +180,11 @@ end
 # ╠═dd455500-d1bf-443d-b589-d400b6844874
 # ╠═7e6ec3b2-a8a7-42a0-87fe-b069c5a66a46
 # ╠═0447ca7d-9dc0-4777-b4ac-adc2eb7d3c8a
+# ╠═a1c1b45f-8692-4456-ab20-d72b3e44fc0d
+# ╠═f039dc58-04ba-4682-8f87-86ec19bd8d2f
+# ╠═413e1cd3-00cf-4bb3-98f2-35fa323454cd
 # ╠═32d1b83e-acfa-4351-9bcf-bc4367781186
 # ╟─b94c0e24-1793-4fe0-b84d-974d0c27113c
 # ╠═c81dfd67-d338-43af-82b4-a83671c3148d
-# ╠═8accf027-0261-42e5-ac11-c066cfb57c43
+# ╟─8accf027-0261-42e5-ac11-c066cfb57c43
+# ╠═bcb92a18-166c-46a7-aace-ccca97a825e4
