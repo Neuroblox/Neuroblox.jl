c14c25f389169eb6daefc21737fe8e915f131145
Helmut Strey <Helmut.Strey@gmail.com>
2022-04-25 17:39:01 -0400

    fix error that David found in creating circuits

--- examples/eightregions.jl
+++ examples/eightregions.jl
@@ -18,9 +18,12 @@ end
 begin
     import Pkg
     # activate the shared project environment
-    Pkg.activate()
+    Pkg.activate(@__DIR__)
     # instantiate, i.e. make sure that all packages are downloaded
     Pkg.instantiate()
+	Pkg.add("OrdinaryDiffEq")
+	Pkg.add("MetaGraphs")
+	Pkg.add("Graphs")
 	Pkg.add("Plots")
 	Pkg.add(path=joinpath(@__DIR__, "..", "..", "Neuroblox.jl"))
 	Pkg.add("OrdinaryDiffEq")
@@ -30,14 +33,14 @@ end
 # ╔═╡ 66460e85-1544-406b-9c79-773ab174a5cb
 begin
 	# Create Regions
-	@named Str = jansen_ritC(τ=0.0022, H=20, λ=300, r=0.3)
-	@named GPe = jansen_ritC(τ=0.04, H=20, λ=400, r=0.1)
-	@named STN = jansen_ritC(τ=0.01, H=20, λ=500, r=0.1)
+	@named Str = jansen_ritSC(τ=0.0022, H=20, λ=300, r=0.3)
+	@named GPe = jansen_ritSC(τ=0.04, H=20, λ=400, r=0.1)
+	@named STN = jansen_ritSC(τ=0.01, H=20, λ=500, r=0.1)
 	@named GPi = jansen_ritSC(τ=0.014, H=20, λ=400, r=0.1)
 	@named Th  = jansen_ritSC(τ=0.002, H=10, λ=20, r=5)
-	@named EI  = jansen_ritSC(τ=0.01, H=20, λ=5, r=5)
-	@named PY  = jansen_ritSC(τ=0.001, H=20, λ=5, r=0.15)
-	@named II  = jansen_ritSC(τ=2.0, H=60, λ=5, r=5)
+	@named EI  = jansen_ritC(τ=0.01, H=20, λ=5, r=5)
+	@named PY  = jansen_ritC(τ=0.001, H=20, λ=5, r=0.15)
+	@named II  = jansen_ritC(τ=2.0, H=60, λ=5, r=5)
 end
 
 # ╔═╡ 9aa9ae2b-b8a0-463d-8e9e-b3339b25a99d
@@ -74,7 +77,7 @@ begin
 	add_edge!(g,7,8,:weight, -1.5*C_Cor)
 
 	add_edge!(g,8,7,:weight, 1.5*C_Cor)
-	add_edge!(g,8,8,:weight, 3.3*C_Cor)
+	add_edge!(g,8,8,:weight, -3.3*C_Cor)
 end
 
 # ╔═╡ 5fc63975-3a15-4430-a7f1-4e5db64c04a1
@@ -88,13 +91,33 @@ adj_matrix_lin = [0 0 0 0 0 0 0 0;
             0 0 0 -0.5*C_BG_Th 0 0 0 0;
             0 0 0 0 C_BG_Th_Cor 0 6*C_Cor 0;
             0 0 0 0 0 4.8*C_Cor 0 -1.5*C_Cor;
-            0 0 0 0 0 0 1.5*C_Cor 3.3*C_Cor]
+            0 0 0 0 0 0 1.5*C_Cor -3.3*C_Cor]
+
+# ╔═╡ a5faad0e-8db0-4bef-b344-355b5cc8ef0f
+begin
+	blox = [ get_prop(g.graph,v,:blox) for v in 1:nv(g.graph)]
+    sys = [s.odesystem for s in blox]
+    connector = [s.connector for s in blox]
+end
+
+# ╔═╡ 6d4463a3-a13a-4adb-950f-0e2115d92b8f
+blox
+
+# ╔═╡ 3ae6df4c-25cb-46b9-940c-4cd5a811689e
+begin
+	adj = adj_matrix_lin .* connector
+    eqs = []
+    for region_num in 1:length(sys)
+            push!(eqs, sys[region_num].jcn ~ sum(adj[:,region_num]))
+    end
+	@named eightregions_odesys = ODESystem(eqs, systems = sys)
+end
 
 # ╔═╡ 33002a2b-f8a9-4728-8288-2f92d3b89948
 @named eight_regions_gr = ODEfromGraph(g=g)
 
 # ╔═╡ c4b4aa78-0324-4ea8-9903-efe87f6074e8
-eight_regions_s = structural_simplify(eight_regions_gr)
+eight_regions_s = structural_simplify(eightregions_odesys)
 
 # ╔═╡ 1a48d894-f43b-4559-8844-50b6e1989bda
 sim_dur = 10.0 # Simulate for 10 Seconds
@@ -131,7 +154,7 @@ begin
 	p_new[2] = (corbgth-5000)/1000
 	p_new[3] = (cor)/100
 	p_new[4] = (bgthcor-5000)/1000
-	prob_new = remake(prob; p=p_new)
+	prob_new = remake(prob; p=p_new, u0=ones(16)*0.1)
 	sol = solve(prob_new, Tsit5())
 end
 
@@ -140,14 +163,22 @@ end
 
 # ╔═╡ 8accf027-0261-42e5-ac11-c066cfb57c43
 begin
-	plot(sol.t,sol[1,:])
-	plot!(sol.t,sol[3,:])
-	plot!(sol.t,sol[5,:])
-	plot!(sol.t,sol[7,:])
-	plot!(sol.t,sol[9,:])
-	plot!(sol.t,sol[11,:])
-	plot!(sol.t,sol[13,:])
-	plot!(sol.t,sol[15,:])
+	l = @layout [a; b; c]
+	p1 = plot(sol.t,sol[11,:],label="EI")
+	p2 = plot(sol.t,sol[13,:],label="PY")
+	p3 = plot(sol.t,sol[15,:],label="II")
+	plot(p1, p2, p3, layout = l)
+end
+
+# ╔═╡ bcb92a18-166c-46a7-aace-ccca97a825e4
+begin
+	l2 = @layout [a; b; c; d; e]
+	p4 = plot(sol.t,sol[1,:],label="Str")
+	p5 = plot(sol.t,sol[3,:],label="GPe")
+	p6 = plot(sol.t,sol[5,:],label="STN")
+	p7 = plot(sol.t,sol[7,:],label="GPi")
+	p8 = plot(sol.t,sol[9,:],label="Th")
+	plot(p4, p5, p6, p7, p8, layout = l2)
 end
 
 # ╔═╡ Cell order:
@@ -157,6 +188,9 @@ end
 # ╠═fef8036a-f073-4473-9549-2b69ec644e8a
 # ╠═5fc63975-3a15-4430-a7f1-4e5db64c04a1
 # ╠═785d8e9a-9ec0-4a5d-92a4-1d1d18d4ffc0
+# ╠═a5faad0e-8db0-4bef-b344-355b5cc8ef0f
+# ╠═6d4463a3-a13a-4adb-950f-0e2115d92b8f
+# ╠═3ae6df4c-25cb-46b9-940c-4cd5a811689e
 # ╠═33002a2b-f8a9-4728-8288-2f92d3b89948
 # ╠═c4b4aa78-0324-4ea8-9903-efe87f6074e8
 # ╠═1a48d894-f43b-4559-8844-50b6e1989bda
@@ -168,3 +202,4 @@ end
 # ╟─b94c0e24-1793-4fe0-b84d-974d0c27113c
 # ╠═c81dfd67-d338-43af-82b4-a83671c3148d
 # ╠═8accf027-0261-42e5-ac11-c066cfb57c43
+# ╠═bcb92a18-166c-46a7-aace-ccca97a825e4
