6d1dd4140a2698874341e7f4a1b31e170cc30a25
SageanneS <sageanne.senneff@ucdconnect.ie>
2022-05-06 16:24:45 -0400

    grab mean ARV error due to noise

--- test/controllers.jl
+++ test/controllers.jl
@@ -23,6 +23,6 @@ circular_loc = Neuroblox.PhaseTarget(data, lb, ub, fs)
 Test for ControlError
 """
 control_error_ARV = Neuroblox.ControlError("ARV", data, data, 9, 16, 1000, 0.150)
-@test all(control_error_ARV) .≈ 0
+@test mean(control_error_ARV) .≈ 0
 control_error_phase = Neuroblox.ControlError("phase", data, data, 9, 16, 1000, 0.150)
-@test all(control_error_phase) .≈ 0
+@test mean(control_error_phase) .≈ 0
