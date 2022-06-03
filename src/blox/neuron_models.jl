7be9e4361c037f9c109482d0cb5e0037c63251e0
Helmut Strey <Helmut.Strey@gmail.com>
2022-06-02 22:36:38 -0400

    IFNeuronBlox

--- src/blox/neuron_models.jl
+++ src/blox/neuron_models.jl
@@ -40,7 +40,7 @@ mutable struct IFNeuronBlox <: NeuronBlox
 	τ::Num
     connector::Num
     odesystem::ODESystem
-	function if_neuron(;name,C=1.0,E_syn=0,G_syn=0.2,I_in=0,freq=0,phase=0,τ=10)
+	function IFNeuronBlox(;name,C=1.0,E_syn=0,G_syn=0.2,I_in=0,freq=0,phase=0,τ=10)
 
 		sts = @variables V(t) = -70.00 G(t)=0.0 z(t)=0.0 spt(t)=0 Cₜ(t) = 0 Isyn(t)=0 
 		ps = @parameters C=C I_in = I_in Eₘ = -70.0 Rₘ = 100.0 θ = -50.0 τ₁=0.1 τ₂=τ E_syn=E_syn G_syn=G_syn phase=phase τᵣ=3
