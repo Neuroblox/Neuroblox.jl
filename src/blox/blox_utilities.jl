053142db313b48880308ee174faf8dd92df5eb7e
Helmut Strey <Helmut.Strey@stonybrook.edu>
2023-07-16 18:04:25 -0400

    had to extend type to Real

--- src/blox/blox_utilities.jl
+++ src/blox/blox_utilities.jl
@@ -1,12 +1,9 @@
-function scope_dict(para_dict::Dict{Symbol, Union{Float64,Num}})
-    @show para_dict
+function scope_dict(para_dict::Dict{Symbol, Union{Real,Num}})
     para_dict_copy = copy(para_dict)
     for (n,v) in para_dict_copy
         if typeof(v) == Num
             para_dict_copy[n] = ParentScope(v)
         else
-            @show n
-            @show v
             para_dict_copy[n] = (@parameters $n=v)[1]
         end
     end
