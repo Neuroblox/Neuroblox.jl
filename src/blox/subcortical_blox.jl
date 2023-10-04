5902adddbe595939324d835e2a46838347b92f26
Haris Orgn <organtzh@gmail.com>
2023-10-04 20:52:25 +0300

    fix identation

--- src/blox/subcortical_blox.jl
+++ src/blox/subcortical_blox.jl
@@ -82,51 +82,40 @@ struct GPi <: AbstractComponent
         phase=zeros(N_inhib),
         τ_inhib=70
     )
-    n_inh = [
-        HHNeuronInhibBlox(
-                name = Symbol("inh$i"),
-                namespace = namespaced_name(namespace, name), 
-                E_syn = E_syn_inhib, 
-                G_syn = G_syn_inhib, 
-                τ = τ_inhib,
-                I_in = I_in[i],
-                freq = freq[i],
-                phase = phase[i]
-        ) 
+        n_inh = [
+            HHNeuronInhibBlox(
+                    name = Symbol("inh$i"),
+                    namespace = namespaced_name(namespace, name), 
+                    E_syn = E_syn_inhib, 
+                    G_syn = G_syn_inhib, 
+                    τ = τ_inhib,
+                    I_in = I_in[i],
+                    freq = freq[i],
+                    phase = phase[i]
+            ) 
+            for i in Base.OneTo(N_inhib)
+        ]
+
+        g = MetaDiGraph()
         for i in Base.OneTo(N_inhib)
-    ]
+            add_blox!(g, n_inh[i])
+        end
 
-    g = MetaDiGraph()
-    for i in Base.OneTo(N_inhib)
-        add_blox!(g, n_inh[i])
-    end
+        parts = n_inh
 
-    parts = n_inh
-    # Construct a BloxConnector object from the graph
-    # containing all connection equations from lower levels and this level.
-    bc = connector_from_graph(g)
-    # If a namespace is not provided, assume that this is the highest level
-    # and construct the ODEsystem from the graph.
-    # If there is a higher namespace, construct only a subsystem containing the parts of this level
-    # and propagate the BloxConnector object `bc` to the higher level 
-    # to potentially add more terms to the same connections.
-    sys = isnothing(namespace) ? system_from_graph(g, bc; name) : system_from_parts(parts; name)
+        bc = connector_from_graph(g)
     
-    # TO DO : m is a subset of states to be plotted in the GUI. 
-    # This can be moved to NeurobloxGUI, maybe via plotting recipes, 
-    # since it is not an essential part of the blox.
-    m = if isnothing(namespace) 
-        [s for s in states.((sys,), states(sys)) if contains(string(s), "V(t)")]
-    else
-        @variables t
-        # HACK : Need to define an empty system to add the correct namespace to states.
-        # Adding a dispatch `ModelingToolkit.states(::Symbol, ::AbstractArray)` upstream will solve this.
-        sys_namespace = System(Equation[], t; name=namespaced_name(namespace, name))
-        [s for s in states.((sys_namespace,), states(sys)) if contains(string(s), "V(t)")]
-    end
+        sys = isnothing(namespace) ? system_from_graph(g, bc; name) : system_from_parts(parts; name)
+        
+        m = if isnothing(namespace) 
+            [s for s in states.((sys,), states(sys)) if contains(string(s), "V(t)")]
+        else
+            @variables t
+            sys_namespace = System(Equation[], t; name=namespaced_name(namespace, name))
+            [s for s in states.((sys_namespace,), states(sys)) if contains(string(s), "V(t)")]
+        end
 
-    new(namespace, parts, sys, bc, m)
-
+        new(namespace, parts, sys, bc, m)
     end
 
 end    
@@ -151,50 +140,40 @@ struct GPe <: AbstractComponent
         phase=zeros(N_inhib),
         τ_inhib=70
     )
-    n_inh = [
-        HHNeuronInhibBlox(
-                name = Symbol("inh$i"),
-                namespace = namespaced_name(namespace, name), 
-                E_syn = E_syn_inhib, 
-                G_syn = G_syn_inhib, 
-                τ = τ_inhib,
-                I_in = I_in[i],
-                freq = freq[i],
-                phase = phase[i]
-        ) 
+        n_inh = [
+            HHNeuronInhibBlox(
+                    name = Symbol("inh$i"),
+                    namespace = namespaced_name(namespace, name), 
+                    E_syn = E_syn_inhib, 
+                    G_syn = G_syn_inhib, 
+                    τ = τ_inhib,
+                    I_in = I_in[i],
+                    freq = freq[i],
+                    phase = phase[i]
+            ) 
+            for i in Base.OneTo(N_inhib)
+        ]
+
+        g = MetaDiGraph()
         for i in Base.OneTo(N_inhib)
-    ]
+            add_blox!(g, n_inh[i])
+        end
 
-    g = MetaDiGraph()
-    for i in Base.OneTo(N_inhib)
-        add_blox!(g, n_inh[i])
-    end
+        parts = n_inh
 
-    parts = n_inh
-    # Construct a BloxConnector object from the graph
-    # containing all connection equations from lower levels and this level.
-    bc = connector_from_graph(g)
-    # If a namespace is not provided, assume that this is the highest level
-    # and construct the ODEsystem from the graph.
-    # If there is a higher namespace, construct only a subsystem containing the parts of this level
-    # and propagate the BloxConnector object `bc` to the higher level 
-    # to potentially add more terms to the same connections.
-    sys = isnothing(namespace) ? system_from_graph(g, bc; name) : system_from_parts(parts; name)
+        bc = connector_from_graph(g)
     
-    # TO DO : m is a subset of states to be plotted in the GUI. 
-    # This can be moved to NeurobloxGUI, maybe via plotting recipes, 
-    # since it is not an essential part of the blox.
-    m = if isnothing(namespace) 
-        [s for s in states.((sys,), states(sys)) if contains(string(s), "V(t)")]
-    else
-        @variables t
-        # HACK : Need to define an empty system to add the correct namespace to states.
-        # Adding a dispatch `ModelingToolkit.states(::Symbol, ::AbstractArray)` upstream will solve this.
-        sys_namespace = System(Equation[], t; name=namespaced_name(namespace, name))
-        [s for s in states.((sys_namespace,), states(sys)) if contains(string(s), "V(t)")]
-    end
+        sys = isnothing(namespace) ? system_from_graph(g, bc; name) : system_from_parts(parts; name)
+        
+        m = if isnothing(namespace) 
+            [s for s in states.((sys,), states(sys)) if contains(string(s), "V(t)")]
+        else
+            @variables t
+            sys_namespace = System(Equation[], t; name=namespaced_name(namespace, name))
+            [s for s in states.((sys_namespace,), states(sys)) if contains(string(s), "V(t)")]
+        end
 
-    new(namespace, parts, sys, bc, m)
+        new(namespace, parts, sys, bc, m)
 
     end
 
@@ -220,51 +199,40 @@ struct Thalamus <: AbstractComponent
         phase=zeros(N_exci),
         τ_exci=5
     )
-    n_exci = [
-        HHNeuronExciBlox(
-                name = Symbol("exci$i"),
-                namespace = namespaced_name(namespace, name), 
-                E_syn = E_syn_exci, 
-                G_syn = G_syn_exci, 
-                τ = τ_exci,
-                I_in = I_in[i],
-                freq = freq[i],
-                phase = phase[i]
-        ) 
+        n_exci = [
+            HHNeuronExciBlox(
+                    name = Symbol("exci$i"),
+                    namespace = namespaced_name(namespace, name), 
+                    E_syn = E_syn_exci, 
+                    G_syn = G_syn_exci, 
+                    τ = τ_exci,
+                    I_in = I_in[i],
+                    freq = freq[i],
+                    phase = phase[i]
+            ) 
+            for i in Base.OneTo(N_exci)
+        ]
+
+        g = MetaDiGraph()
         for i in Base.OneTo(N_exci)
-    ]
+            add_blox!(g, n_exci[i])
+        end
 
-    g = MetaDiGraph()
-    for i in Base.OneTo(N_exci)
-        add_blox!(g, n_exci[i])
-    end
-
-    parts = n_exci
-    # Construct a BloxConnector object from the graph
-    # containing all connection equations from lower levels and this level.
-    bc = connector_from_graph(g)
-    # If a namespace is not provided, assume that this is the highest level
-    # and construct the ODEsystem from the graph.
-    # If there is a higher namespace, construct only a subsystem containing the parts of this level
-    # and propagate the BloxConnector object `bc` to the higher level 
-    # to potentially add more terms to the same connections.
-    sys = isnothing(namespace) ? system_from_graph(g, bc; name) : system_from_parts(parts; name)
+        parts = n_exci
     
-    # TO DO : m is a subset of states to be plotted in the GUI. 
-    # This can be moved to NeurobloxGUI, maybe via plotting recipes, 
-    # since it is not an essential part of the blox.
-    m = if isnothing(namespace) 
-        [s for s in states.((sys,), states(sys)) if contains(string(s), "V(t)")]
-    else
-        @variables t
-        # HACK : Need to define an empty system to add the correct namespace to states.
-        # Adding a dispatch `ModelingToolkit.states(::Symbol, ::AbstractArray)` upstream will solve this.
-        sys_namespace = System(Equation[], t; name=namespaced_name(namespace, name))
-        [s for s in states.((sys_namespace,), states(sys)) if contains(string(s), "V(t)")]
-    end
+        bc = connector_from_graph(g)
+        
+        sys = isnothing(namespace) ? system_from_graph(g, bc; name) : system_from_parts(parts; name)
+        
+        m = if isnothing(namespace) 
+            [s for s in states.((sys,), states(sys)) if contains(string(s), "V(t)")]
+        else
+            @variables t
+            sys_namespace = System(Equation[], t; name=namespaced_name(namespace, name))
+            [s for s in states.((sys_namespace,), states(sys)) if contains(string(s), "V(t)")]
+        end
 
-    new(namespace, parts, sys, bc, m)
-
+        new(namespace, parts, sys, bc, m)
     end
 
 end   
