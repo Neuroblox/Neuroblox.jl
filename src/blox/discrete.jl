ac419873cd8187ac87c7d968b7c2ba93019cfad0
Haris Orgn <organtzh@gmail.com>
2023-11-03 22:27:35 +0200

    remove algebraic states from subcortical discrete blox

--- src/blox/discrete.jl
+++ src/blox/discrete.jl
@@ -2,18 +2,18 @@ abstract type AbstractAlgebraic end
 
 abstract type AbstractModulator <: AbstractAlgebraic end
 
+linear_func(jcn, H) = H*jcn
+@register_symbolic linear_func(jcn, H)
+
 struct Matrisome <: AbstractAlgebraic
     odesystem
     namespace
 
     function Matrisome(; name, namespace=nothing)
         @variables t 
-        sts = @variables ρ(t)=0.0 [irreducible=true] jcn(t)=0.0 [input=true]
+        sts = @variables jcn(t)=0.0 [input=true, irreducible=true]
         ps = @parameters H=1
-        eqs = [
-            ρ ~ H*jcn
-        ]
-        sys = ODESystem(eqs, t, sts, ps; name)
+        sys = ODESystem(Equation[], t, sts, ps; name)
 
         new(sys, namespace)
     end
@@ -25,16 +25,15 @@ struct Striosome <: AbstractAlgebraic
 
     function Striosome(; name, namespace=nothing)
         @variables t 
-        sts = @variables ρ(t)=0.0 [irreducible=true] jcn(t)=0.0 [input=true]
+        sts = @variables jcn(t)=0.0 [input=true]
         ps = @parameters H=1
-        eqs = [
-            ρ ~ H*jcn + 0.1
-        ]
-        sys = ODESystem(eqs, t, sts, ps; name)
+        sys = ODESystem(Equation[], t, sts, ps; name)
 
         new(sys, namespace)
     end
 end
+
+cases_func(jcn, κ) = IfElse.ifelse(iszero(jcn), κ, κ/jcn)
 
 struct TAN <: AbstractAlgebraic
     odesystem
@@ -42,12 +41,9 @@ struct TAN <: AbstractAlgebraic
 
     function TAN(; name, namespace=nothing, κ=0.2)
         @variables t 
-        sts = @variables R(t)=κ [irreducible=true] jcn(t)=0.0 [input=true]
+        sts = @variables jcn(t)=0.0 [input=true, irreducible=true]
         ps = @parameters κ=κ spikes_window=0.0
-        eqs = [
-            R ~ IfElse.ifelse(iszero(jcn), κ, κ/jcn)
-        ]
-        sys = ODESystem(eqs, t, sts, ps; name)
+        sys = ODESystem(Equation[], t, sts, ps; name)
 
         new(sys, namespace)
     end
@@ -62,12 +58,9 @@ struct SNc <: AbstractModulator
 
     function SNc(; name, namespace=nothing, κ_DA=0.2, N_time_blocks=5, DA_reward=10)
         @variables t 
-        sts = @variables R(t)=κ_DA jcn(t)=0.0 [input=true]
+        sts = @variables jcn(t)=0.0 [input=true]
         ps = @parameters κ=κ_DA
-        eqs = [
-            R ~ IfElse.ifelse(iszero(jcn), κ, κ/jcn)
-        ]
-        sys = ODESystem(eqs, t, sts, ps; name)
+        sys = ODESystem(Equation[], t, sts, ps; name)
 
         new(sys, N_time_blocks, κ_DA, DA_reward, namespace)
     end
@@ -75,7 +68,14 @@ end
 
 (b::SNc)(R_DA, feedback) = b.N_time_blocks * b.κ_DA + R_DA - b.κ_DA + feedback * b.DA_reward
 
-function get_modulator_state(s::SNc)
+function get_modulator_value(s::SNc, sol, t)
+    jcn = get_modulator_state(s, sol, t)
+
+    return cases_func(jcn, s.κ_DA)
+end
+
+function get_modulator_state(s::SNc, sol, t)
     sys = get_namespaced_sys(s)
-    return sys.R
+
+    return only(sol(t; idxs = [sys.jcn]))
 end
