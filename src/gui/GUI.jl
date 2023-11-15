f204a3ac44720ccfe75a1a4f9a8a70f841421cb7
Helmut Strey <Helmut.Strey@stonybrook.edu>
2023-11-15 15:16:52 -0500

    added NGNM and Cortical to GUI - works in the GUI

--- src/gui/GUI.jl
+++ src/gui/GUI.jl
@@ -294,12 +294,24 @@ end
 
 function arguments(::Type{Neuroblox.NextGenerationEIBlox})
   OrderedDict(
-    :C => NCAD(30.0, NUMBER, 1.0, 50.0,[]),
-    :Δ => NCAD(1.0, NUMBER, 0.01, 100.0,[]),
-    :η_0 => NCAD(5.0, NUMBER, 0.01, 20.0,[]),
-    :v_syn => NCAD(-10.0, NUMBER, -20.0, 0.0,[]),
-    :alpha_inv => NCAD(35.0, NUMBER, 0.01, 10.0,[]),
-    :k => NCAD(0.105, NUMBER, 0.01, 2.0,[])
+    :Cₑ => NCAD(52.0, NUMBER, 1.0, 50.0,[]),
+    :Cᵢ => NCAD(26.0, NUMBER, 1.0, 50.0,[]),
+    :Δₑ => NCAD(0.5, NUMBER, 0.01, 100.0,[]),
+    :Δᵢ => NCAD(0.5, NUMBER, 0.01, 100.0,[]),
+    :η_0ₑ => NCAD(10.0, NUMBER, 0.01, 20.0,[]),
+    :η_0ᵢ => NCAD(0.0, NUMBER, 0.01, 20.0,[]),
+    :v_synₑₑ => NCAD(10.0, NUMBER, -20.0, 20.0,[]),
+    :v_synₑᵢ => NCAD(-10.0, NUMBER, -20.0, 20.0,[]),
+    :v_synᵢₑ => NCAD(10.0, NUMBER, -20.0, 20.0,[]),
+    :v_synᵢᵢ => NCAD(-10.0, NUMBER, -20.0, 20.0,[]),
+    :alpha_invₑₑ => NCAD(10.0/26, NUMBER, 0.01, 20.0,[]),
+    :alpha_invₑᵢ => NCAD(0.8/26, NUMBER, 0.01, 20.0,[]),
+    :alpha_invᵢₑ => NCAD(10.0/26, NUMBER, 0.01, 20.0,[]),
+    :alpha_invᵢᵢ => NCAD(0.8/26, NUMBER, 0.01, 20.0,[]),
+    :kₑₑ => NCAD(0.0, NUMBER, 0.01, 20.0,[]),
+    :kₑᵢ => NCAD(0.6*26, NUMBER, 0.01, 20.0,[]),
+    :kᵢₑ => NCAD(0.6*26, NUMBER, 0.01, 20.0,[]),
+    :kᵢᵢ => NCAD(0.0, NUMBER, 0.01, 20.0,[])
   )
 end
 
@@ -420,7 +432,9 @@ function arguments(::Type{Neuroblox.CorticalBlox}) #TODO: add correct settings f
     :phase => NCAD(0, NUMBER, 0.0, 2*π,[]),
     :I_bg_ar => NCAD(0.0, NUMBER, 0.01, 20.0,[]),
 	  :τ_exci => NCAD(5, NUMBER, 0.01, 20.0,[]),
-	  :τ_inhib => NCAD(70, NUMBER, 0.01, 100.0,[])
+	  :τ_inhib => NCAD(70, NUMBER, 0.01, 100.0,[]),
+    :density => NCAD(0.1, NUMBER, 0.01, 1.0,[]),
+    :weight => NCAD(1.0, NUMBER, 0.01, 100.0,[])
   )
 end
 
