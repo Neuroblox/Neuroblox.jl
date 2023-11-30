24cc6e59920af2e447e4a178d325da459561af0c
Helmut Strey <Helmut.Strey@stonybrook.edu>
2023-11-30 10:56:25 -0500

    fixed GUI.jl

--- src/gui/GUI.jl
+++ src/gui/GUI.jl
@@ -12,6 +12,7 @@ Base.@kwdef struct NeurobloxConstructorArgumentData
   min_value
   max_value
   menuitems
+  isparam
 end
 const NCAD = NeurobloxConstructorArgumentData
 
@@ -75,24 +76,25 @@ const MENU = "menu"
 
 function arguments(::Type{Neuroblox.ImageStimulus})
   OrderedDict(
-    :height => NCAD(25, INTEGER, 1, 100,[]),
-    :width => NCAD(25, INTEGER, 1, 100,[]),
-    :dt => NCAD(0.6, NUMBER, 0.1, 10,[])
+    :height => NCAD(25, INTEGER, 1, 100,[], false),
+    :width => NCAD(25, INTEGER, 1, 100,[], false),
+    :t_stimulus => NCAD(0.6, NUMBER, 0.1, 10,[],true),
+    :t_pause => NCAD(0.6, NUMBER, 0.1, 10,[],true)
   )
 end
 
 function arguments(::Type{Neuroblox.WinnerTakeAllBlox})
   OrderedDict(
     # :BlockSize => NCAD(5, INTEGER, 1, 40, []),
-    :E_syn_exci => NCAD(1.0, NUMBER, 0.01, 100.0,[]),
-    :G_syn_exci => NCAD(5.0, NUMBER, 0.01, 20.0,[]),
-    :I_in => NCAD(0.0, NUMBER, 0.01, 20.0,[]),
-    :phase => NCAD(0, NUMBER, 0.0, 2*π,[]),
-	  :τ_exci => NCAD(0.105, NUMBER, 0.01, 2.0,[]),
-    :E_syn_inhib => NCAD(1.0, NUMBER, 0.01, 100.0,[]),
-    :G_syn_inhib => NCAD(5.0, NUMBER, 0.01, 20.0,[]),
-    :I_in => NCAD(0.0, NUMBER, 0.01, 20.0,[]),
-	  :τ_inhib => NCAD(0.105, NUMBER, 0.01, 2.0,[])
+    :E_syn_exci => NCAD(1.0, NUMBER, 0.01, 100.0,[],true),
+    :G_syn_exci => NCAD(5.0, NUMBER, 0.01, 20.0,[],true),
+    :I_in => NCAD(0.0, NUMBER, 0.01, 20.0,[],true),
+    :phase => NCAD(0, NUMBER, 0.0, 2*π,[],true),
+	  :τ_exci => NCAD(0.105, NUMBER, 0.01, 2.0,[],true),
+    :E_syn_inhib => NCAD(1.0, NUMBER, 0.01, 100.0,[],true),
+    :G_syn_inhib => NCAD(5.0, NUMBER, 0.01, 20.0,[],true),
+    :I_in => NCAD(0.0, NUMBER, 0.01, 20.0,[],true),
+	  :τ_inhib => NCAD(0.105, NUMBER, 0.01, 2.0,[],true)
   )
 end
 
@@ -146,10 +148,10 @@ function arguments(::Type{Neuroblox.HarmonicOscillator})
   OrderedDict(
     #:measurement => NCAD("Measurement", MENU, 1 , 4 ,["none","fMRI", "EEG", "LFP"]),
     # MENU NCAD("title of menue", MENU, default, #ofoptions, list of options)
-    :ω => NCAD(25*(2*pi), NUMBER, (2*pi), 150*(2*pi),[]),
-    :ζ => NCAD(1.0, NUMBER, -1.0, 1.0,[]),
-    :k => NCAD(625*(2*pi), NUMBER, (2*pi), 22500*(2*pi),[]),
-    :h => NCAD(35.0, NUMBER, 0.01, 90.0,[])
+    :ω => NCAD(25*(2*pi), NUMBER, (2*pi), 150*(2*pi),[],true),
+    :ζ => NCAD(1.0, NUMBER, -1.0, 1.0,[],true),
+    :k => NCAD(625*(2*pi), NUMBER, (2*pi), 22500*(2*pi),[],true),
+    :h => NCAD(35.0, NUMBER, 0.01, 90.0,[],true)
   )
 end
 
@@ -177,10 +179,10 @@ end
 
 function arguments(::Type{Neuroblox.JansenRit})
   OrderedDict(
-    :τ => NCAD(0.014, NUMBER, 0.001, 0.1,[]),
-    :H => NCAD(20.0, NUMBER, 0.0, 500.0,[]),
-    :λ => NCAD(400.0, NUMBER, 20.0, 500.0,[]),
-    :r => NCAD(0.1, NUMBER, 0.1, 5.0,[])
+    :τ => NCAD(0.014, NUMBER, 0.001, 0.1,[],true),
+    :H => NCAD(20.0, NUMBER, 0.0, 500.0,[],true),
+    :λ => NCAD(400.0, NUMBER, 20.0, 500.0,[],true),
+    :r => NCAD(0.1, NUMBER, 0.1, 5.0,[],true)
   )
 end
 
@@ -206,17 +208,17 @@ end
 
 function arguments(::Type{Neuroblox.WilsonCowan})
   OrderedDict(
-    :τ_E => NCAD(1.0, NUMBER, 1.0, 100.0,[]),
-    :τ_I => NCAD(1.0, NUMBER, 1.0, 100.0,[]),
-    :a_E => NCAD(1.2, NUMBER, 1.0, 100.0,[]),
-    :a_I => NCAD(2.0, NUMBER, 1.0, 100.0,[]),
-    :c_EE => NCAD(5.0, NUMBER, 1.0, 100.0,[]),
-    :c_EI => NCAD(10.0, NUMBER, 1.0, 100.0,[]),
-    :c_IE => NCAD(6.0, NUMBER, 1.0, 100.0,[]),
-    :c_II => NCAD(1.0, NUMBER, 1.0, 100.0,[]),
-    :θ_E => NCAD(2.0, NUMBER, 1.0, 100.0,[]),
-    :θ_I => NCAD(3.5, NUMBER, 1.0, 100.0,[]),
-    :η => NCAD(1.0, NUMBER, 1.0, 100.0,[])
+    :τ_E => NCAD(1.0, NUMBER, 1.0, 100.0,[],true),
+    :τ_I => NCAD(1.0, NUMBER, 1.0, 100.0,[],true),
+    :a_E => NCAD(1.2, NUMBER, 1.0, 100.0,[],true),
+    :a_I => NCAD(2.0, NUMBER, 1.0, 100.0,[],true),
+    :c_EE => NCAD(5.0, NUMBER, 1.0, 100.0,[],true),
+    :c_EI => NCAD(10.0, NUMBER, 1.0, 100.0,[],true),
+    :c_IE => NCAD(6.0, NUMBER, 1.0, 100.0,[],true),
+    :c_II => NCAD(1.0, NUMBER, 1.0, 100.0,[],true),
+    :θ_E => NCAD(2.0, NUMBER, 1.0, 100.0,[],true),
+    :θ_I => NCAD(3.5, NUMBER, 1.0, 100.0,[],true),
+    :η => NCAD(1.0, NUMBER, 1.0, 100.0,[],true)
   )
 end
 
@@ -259,34 +261,34 @@ end
 
 function arguments(::Type{Neuroblox.LarterBreakspear})
   OrderedDict(
-    :C => NCAD(0.35, NUMBER, 0.0, 1.0,[]),
-    :δ_VZ => NCAD(0.61, NUMBER, 0.1, 2.0,[]),
-    :T_Ca => NCAD(-0.01, NUMBER, 0.02, -0.04,[]),
-    :δ_Ca => NCAD(0.15, NUMBER, 0.1, 0.2,[]),
-    :g_Ca => NCAD(1.0, NUMBER, 0.96, 1.01,[]), #tested in Jolien's work/similar to V_Ca in Anthony's paper
-    :V_Ca => NCAD(1.0, NUMBER, 0.96, 1.01,[]), #limits established by bifurcation
-    :T_K => NCAD(0.0, NUMBER, -0.05, 0.05,[]),
-    :δ_K => NCAD(0.3, NUMBER,0.25, 0.35,[]),
-    :g_K => NCAD(2.0, NUMBER, 1.95, 2.05,[]),  #tested in Jolien's work
-    :V_K => NCAD(-0.7, NUMBER, -0.8, -0.6,[]),  #limits established by bifurcation
-    :T_Na => NCAD(0.3, NUMBER, 0.25, 0.35,[]),
-    :δ_Na => NCAD(0.15, NUMBER, 0.1, 0.2,[]),
-    :g_Na => NCAD(6.7, NUMBER, 6.6, 6.8,[]),   #tested in Botond and Jolien's work
-    :V_Na => NCAD(0.53, NUMBER, 0.41, 0.59,[]), #limits established by bifurcation
-    :V_L => NCAD(-0.5, NUMBER, -0.6, -0.4,[]),
-    :g_L => NCAD(0.5, NUMBER, 0.4, 0.6,[]),
-    :V_T => NCAD(0.0, NUMBER, -0.05, 0.05,[]),
-    :Z_T => NCAD(0.0, NUMBER, -0.05, 0.05,[]),
-    :IS => NCAD(0.3, NUMBER, 0.0, 1.0,[]),
-    :a_ee => NCAD(0.36, NUMBER, 0.33, 0.39,[]), #tested in Botond and Jolien's work
-    :a_ei => NCAD(2.0, NUMBER, 1.95, 2.05,[]), #tested in Botond and Jolien's work
-    :a_ie => NCAD(2.0, NUMBER, 1.95, 2.05,[]), #testing in Jolien's work
-    :a_ne => NCAD(1.0, NUMBER, 0.95, 1.05,[]),
-    :a_ni => NCAD(0.4, NUMBER, 0.3, 0.5,[]),
-    :b => NCAD(0.1, NUMBER, 0.05, 0.15,[]),
-    :τ_K => NCAD(1.0, NUMBER, 0.8, 1.2,[]), #shouldn't be varied, but useful in bifurcations to "harshen" the potassium landscape
-    :ϕ => NCAD(0.7, NUMBER, 0.6, 0.8,[]),
-    :r_NMDA => NCAD( 0.25, NUMBER, 0.2, 0.3,[]) #tested in Botond's work
+    :C => NCAD(0.35, NUMBER, 0.0, 1.0,[],true),
+    :δ_VZ => NCAD(0.61, NUMBER, 0.1, 2.0,[],true),
+    :T_Ca => NCAD(-0.01, NUMBER, 0.02, -0.04,[],true),
+    :δ_Ca => NCAD(0.15, NUMBER, 0.1, 0.2,[],true),
+    :g_Ca => NCAD(1.0, NUMBER, 0.96, 1.01,[],true), #tested in Jolien's work/similar to V_Ca in Anthony's paper
+    :V_Ca => NCAD(1.0, NUMBER, 0.96, 1.01,[],true), #limits established by bifurcation
+    :T_K => NCAD(0.0, NUMBER, -0.05, 0.05,[],true),
+    :δ_K => NCAD(0.3, NUMBER,0.25, 0.35,[],true),
+    :g_K => NCAD(2.0, NUMBER, 1.95, 2.05,[],true),  #tested in Jolien's work
+    :V_K => NCAD(-0.7, NUMBER, -0.8, -0.6,[],true),  #limits established by bifurcation
+    :T_Na => NCAD(0.3, NUMBER, 0.25, 0.35,[],true),
+    :δ_Na => NCAD(0.15, NUMBER, 0.1, 0.2,[],true),
+    :g_Na => NCAD(6.7, NUMBER, 6.6, 6.8,[],true),   #tested in Botond and Jolien's work
+    :V_Na => NCAD(0.53, NUMBER, 0.41, 0.59,[],true), #limits established by bifurcation
+    :V_L => NCAD(-0.5, NUMBER, -0.6, -0.4,[],true),
+    :g_L => NCAD(0.5, NUMBER, 0.4, 0.6,[],true),
+    :V_T => NCAD(0.0, NUMBER, -0.05, 0.05,[],true),
+    :Z_T => NCAD(0.0, NUMBER, -0.05, 0.05,[],true),
+    :IS => NCAD(0.3, NUMBER, 0.0, 1.0,[],true),
+    :a_ee => NCAD(0.36, NUMBER, 0.33, 0.39,[],true), #tested in Botond and Jolien's work
+    :a_ei => NCAD(2.0, NUMBER, 1.95, 2.05,[],true), #tested in Botond and Jolien's work
+    :a_ie => NCAD(2.0, NUMBER, 1.95, 2.05,[],true), #testing in Jolien's work
+    :a_ne => NCAD(1.0, NUMBER, 0.95, 1.05,[],true),
+    :a_ni => NCAD(0.4, NUMBER, 0.3, 0.5,[],true),
+    :b => NCAD(0.1, NUMBER, 0.05, 0.15,[],true),
+    :τ_K => NCAD(1.0, NUMBER, 0.8, 1.2,[],true), #shouldn't be varied, but useful in bifurcations to "harshen" the potassium landscape
+    :ϕ => NCAD(0.7, NUMBER, 0.6, 0.8,[],true),
+    :r_NMDA => NCAD( 0.25, NUMBER, 0.2, 0.3,[],true) #tested in Botond's work
   )
 end
 
@@ -307,24 +309,24 @@ end
 
 function arguments(::Type{Neuroblox.NextGenerationEIBlox})
   OrderedDict(
-    :Cₑ => NCAD(52.0, NUMBER, 1.0, 50.0,[]),
-    :Cᵢ => NCAD(26.0, NUMBER, 1.0, 50.0,[]),
-    :Δₑ => NCAD(0.5, NUMBER, 0.01, 100.0,[]),
-    :Δᵢ => NCAD(0.5, NUMBER, 0.01, 100.0,[]),
-    :η_0ₑ => NCAD(10.0, NUMBER, 0.01, 20.0,[]),
-    :η_0ᵢ => NCAD(0.0, NUMBER, 0.01, 20.0,[]),
-    :v_synₑₑ => NCAD(10.0, NUMBER, -20.0, 20.0,[]),
-    :v_synₑᵢ => NCAD(-10.0, NUMBER, -20.0, 20.0,[]),
-    :v_synᵢₑ => NCAD(10.0, NUMBER, -20.0, 20.0,[]),
-    :v_synᵢᵢ => NCAD(-10.0, NUMBER, -20.0, 20.0,[]),
-    :alpha_invₑₑ => NCAD(10.0/26, NUMBER, 0.01, 20.0,[]),
-    :alpha_invₑᵢ => NCAD(0.8/26, NUMBER, 0.01, 20.0,[]),
-    :alpha_invᵢₑ => NCAD(10.0/26, NUMBER, 0.01, 20.0,[]),
-    :alpha_invᵢᵢ => NCAD(0.8/26, NUMBER, 0.01, 20.0,[]),
-    :kₑₑ => NCAD(0.0, NUMBER, 0.01, 20.0,[]),
-    :kₑᵢ => NCAD(0.6*26, NUMBER, 0.01, 20.0,[]),
-    :kᵢₑ => NCAD(0.6*26, NUMBER, 0.01, 20.0,[]),
-    :kᵢᵢ => NCAD(0.0, NUMBER, 0.01, 20.0,[])
+    :Cₑ => NCAD(52.0, NUMBER, 1.0, 50.0,[],true),
+    :Cᵢ => NCAD(26.0, NUMBER, 1.0, 50.0,[],true),
+    :Δₑ => NCAD(0.5, NUMBER, 0.01, 100.0,[],true),
+    :Δᵢ => NCAD(0.5, NUMBER, 0.01, 100.0,[],true),
+    :η_0ₑ => NCAD(10.0, NUMBER, 0.01, 20.0,[],true),
+    :η_0ᵢ => NCAD(0.0, NUMBER, 0.01, 20.0,[],true),
+    :v_synₑₑ => NCAD(10.0, NUMBER, -20.0, 20.0,[],true),
+    :v_synₑᵢ => NCAD(-10.0, NUMBER, -20.0, 20.0,[],true),
+    :v_synᵢₑ => NCAD(10.0, NUMBER, -20.0, 20.0,[],true),
+    :v_synᵢᵢ => NCAD(-10.0, NUMBER, -20.0, 20.0,[],true),
+    :alpha_invₑₑ => NCAD(10.0/26, NUMBER, 0.01, 20.0,[],true),
+    :alpha_invₑᵢ => NCAD(0.8/26, NUMBER, 0.01, 20.0,[],true),
+    :alpha_invᵢₑ => NCAD(10.0/26, NUMBER, 0.01, 20.0,[],true),
+    :alpha_invᵢᵢ => NCAD(0.8/26, NUMBER, 0.01, 20.0,[],true),
+    :kₑₑ => NCAD(0.0, NUMBER, 0.01, 20.0,[],true),
+    :kₑᵢ => NCAD(0.6*26, NUMBER, 0.01, 20.0,[],true),
+    :kᵢₑ => NCAD(0.6*26, NUMBER, 0.01, 20.0,[],true),
+    :kᵢᵢ => NCAD(0.0, NUMBER, 0.01, 20.0,[],true)
   )
 end
 
@@ -416,14 +418,14 @@ end
 
 function arguments(::Type{Neuroblox.HHNeuronExciBlox}) #TODO: add correct settings for the arguments
   OrderedDict(
-    :t_spike_window => NCAD(90.0, NUMBER, 0.1, 100.0,[]),
-    :θ_spike => NCAD(0.0, NUMBER, 0.0, 2*π,[]),
-    :E_syn => NCAD(0.0, NUMBER, 0.01, 100.0,[]),
-    :G_syn => NCAD(3.0, NUMBER, 0.01, 20.0,[]),
-    :I_bg => NCAD(0.0, NUMBER, 0.0, 20.0,[]),
-    :freq => NCAD(0, NUMBER, 0.0, 100,[]),
-    :phase => NCAD(0, NUMBER, 0.0, 2*π,[]),
-	  :τ => NCAD(5, NUMBER, 0.01, 20.0,[])
+    :t_spike_window => NCAD(90.0, NUMBER, 0.1, 100.0,[],true),
+    :θ_spike => NCAD(0.0, NUMBER, 0.0, 2*π,[],true),
+    :E_syn => NCAD(0.0, NUMBER, 0.01, 100.0,[],true),
+    :G_syn => NCAD(3.0, NUMBER, 0.01, 20.0,[],true),
+    :I_bg => NCAD(0.0, NUMBER, 0.0, 20.0,[],true),
+    :freq => NCAD(0, NUMBER, 0.0, 100,[],true),
+    :phase => NCAD(0, NUMBER, 0.0, 2*π,[],true),
+	  :τ => NCAD(5, NUMBER, 0.01, 20.0,[],true)
   )
 end
 
@@ -433,12 +435,12 @@ end
 
 function arguments(::Type{Neuroblox.HHNeuronInhibBlox}) #TODO: add correct settings for the arguments
   OrderedDict(
-    :E_syn => NCAD(1.0, NUMBER, 0.01, 100.0,[]),
-    :G_syn => NCAD(5.0, NUMBER, 0.01, 20.0,[]),
-    :I_bg => NCAD(0.0, NUMBER, 0.01, 20.0,[]),
-    :freq => NCAD(20, NUMBER, 0.01, 100,[]),
-    :phase => NCAD(0, NUMBER, 0.0, 2*π,[]),
-	  :τ => NCAD(0.105, NUMBER, 0.01, 2.0,[])
+    :E_syn => NCAD(1.0, NUMBER, 0.01, 100.0,[],true),
+    :G_syn => NCAD(5.0, NUMBER, 0.01, 20.0,[],true),
+    :I_bg => NCAD(0.0, NUMBER, 0.01, 20.0,[],true),
+    :freq => NCAD(20, NUMBER, 0.01, 100,[],true),
+    :phase => NCAD(0, NUMBER, 0.0, 2*π,[],true),
+	  :τ => NCAD(0.105, NUMBER, 0.01, 2.0,[],true)
   )
 end
 
@@ -448,20 +450,20 @@ end
 
 function arguments(::Type{Neuroblox.CorticalBlox}) #TODO: add correct settings for the arguments
   OrderedDict(
-    :N_wta => NCAD(10, INTEGER, 1, 100, []),
-    :N_exci => NCAD(5, INTEGER, 1, 100, []),
-    :E_syn_exci => NCAD(0.0, NUMBER, 0.01, 100.0,[]),
-    :E_syn_inhib => NCAD(1.0, NUMBER, 0.01, 100.0,[]),
-    :G_syn_exci => NCAD(3.0, NUMBER, 0.01, 20.0,[]),
-    :G_syn_inhib => NCAD(5.0, NUMBER, 0.01, 20.0,[]),
-    :G_syn_ff_inhib => NCAD(3.5, NUMBER, 0.01, 20.0,[]),
-    :freq => NCAD(0, NUMBER, 0.0, 100,[]),
-    :phase => NCAD(0, NUMBER, 0.0, 2*π,[]),
-    :I_bg_ar => NCAD(0.0, NUMBER, 0.01, 20.0,[]),
-	  :τ_exci => NCAD(5, NUMBER, 0.01, 20.0,[]),
-	  :τ_inhib => NCAD(70, NUMBER, 0.01, 100.0,[]),
-    :density => NCAD(0.1, NUMBER, 0.01, 1.0,[]),
-    :weight => NCAD(1.0, NUMBER, 0.01, 100.0,[])
+    :N_wta => NCAD(10, INTEGER, 1, 100, [],true),
+    :N_exci => NCAD(5, INTEGER, 1, 100, [],true),
+    :E_syn_exci => NCAD(0.0, NUMBER, 0.01, 100.0,[],true),
+    :E_syn_inhib => NCAD(1.0, NUMBER, 0.01, 100.0,[],true),
+    :G_syn_exci => NCAD(3.0, NUMBER, 0.01, 20.0,[],true),
+    :G_syn_inhib => NCAD(5.0, NUMBER, 0.01, 20.0,[],true),
+    :G_syn_ff_inhib => NCAD(3.5, NUMBER, 0.01, 20.0,[],true),
+    :freq => NCAD(0, NUMBER, 0.0, 100,[],true),
+    :phase => NCAD(0, NUMBER, 0.0, 2*π,[],true),
+    :I_bg_ar => NCAD(0.0, NUMBER, 0.01, 20.0,[],true),
+	  :τ_exci => NCAD(5, NUMBER, 0.01, 20.0,[],true),
+	  :τ_inhib => NCAD(70, NUMBER, 0.01, 100.0,[],true),
+    :density => NCAD(0.1, NUMBER, 0.01, 1.0,[],true),
+    :weight => NCAD(1.0, NUMBER, 0.01, 100.0,[],true)
   )
 end
 
@@ -471,17 +473,17 @@ end
 
 function arguments(::Type{Neuroblox.BandPassFilterBlox})
   OrderedDict(
-    :lb => NCAD(10, NUMBER, 0, 500,[]),
-    :ub => NCAD(10, NUMBER, 0, 500,[]),
-    :fs => NCAD(1000, NUMBER, 1, 10000,[]),
-    :order => NCAD(4, INTEGER, 1, 2000,[])
+    :lb => NCAD(10, NUMBER, 0, 500,[],true),
+    :ub => NCAD(10, NUMBER, 0, 500,[],true),
+    :fs => NCAD(1000, NUMBER, 1, 10000,[],true),
+    :order => NCAD(4, INTEGER, 1, 2000,[],true)
   )
 end
 
 function arguments(::Type{Neuroblox.PowerSpectrumBlox})
   OrderedDict(
-    :T => NCAD(10, NUMBER, 0, 2000,[]),
-    :fs => NCAD(1000, NUMBER, 1, 10000,[]),
+    :T => NCAD(10, NUMBER, 0, 2000,[],true),
+    :fs => NCAD(1000, NUMBER, 1, 10000,[],true),
   )
 end
 
