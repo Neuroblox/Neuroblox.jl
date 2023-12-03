ef7cbd58bcd6d7b91d94348bf4fb11f9c7af125e
Helmut Strey <Helmut.Strey@stonybrook.edu>
2023-12-03 00:06:20 -0500

    removed test from RFLearning

--- test/reinforcement_learning.jl
+++ test/reinforcement_learning.jl
@@ -6,7 +6,7 @@ using MetaGraphs
 using DataFrames
 using CSV
 
-@testset "RFLearning with policy" begin
+@testset "RFLearning test" begin
     t_trial = 2 # ms
     time_block_dur = 0.01 # ms
     N_trials = 3
@@ -55,50 +55,6 @@ using CSV
 
     env = ClassificationEnvironment(stim; name=:env, namespace=global_ns)
     run_experiment!(agent, env; alg=QNDF(), reltol=1e-9,abstol=1e-9)
-
-    final_params = agent.problem.p
-    # At least some weights need to be different.
-    @test any(init_params[map_idxs[idxs_weight]] .!= final_params[map_idxs[idxs_weight]])
-    # All non-weight parameters need to be the same.
-    @test all(init_params[map_idxs[idxs_other_params]] .== final_params[map_idxs[idxs_other_params]])
-end
-
-@testset "RFLearning without policy" begin
-    t_trial = 100 # ms
-    time_block_dur = 10 # ms
-    N_trials = 3
-
-    global_ns = :g # global namespace
-    @named VAC = CorticalBlox(N_wta=3, N_exci=3, namespace=global_ns, density=0.1, weight=1)
-    @named PFC = CorticalBlox(N_wta=2, N_exci=3, namespace=global_ns, density=0.1, weight=1)
-
-    fn = "../examples/image_example.csv"
-    data = CSV.read(fn, DataFrame)
-    @named stim = ImageStimulus(data[1:N_trials,:]; namespace=global_ns, t_stimulus=0.4*t_trial, t_pause=0.6*t_trial)
-
-    bloxs = [VAC, PFC, stim]
-
-    d = Dict(b => i for (i,b) in enumerate(bloxs))
-
-    hebbian = HebbianPlasticity(K=0.2, W_lim=2, t_pre=t_trial, t_post=t_trial)
-
-    g = MetaDiGraph()
-    add_blox!.(Ref(g), bloxs)
-
-    add_edge!(g, d[stim], d[VAC], Dict(:weight => 15, :density => 0.1))
-    add_edge!(g, d[VAC], d[PFC], Dict(:weight => 1, :density => 0.1, :learning_rule => hebbian))
-
-    agent = Agent(g; name=:ag);
-    ps = parameters(agent.odesystem)
-    init_params = agent.problem.p
-    map_idxs = Int.(ModelingToolkit.varmap_to_vars([ps[i] => i for i in eachindex(ps)], ps))
-    idxs_weight = findall(x -> occursin("w_", String(Symbol(x))), ps)
-    idx_stim = findall(x -> occursin("stim₊", String(Symbol(x))), ps)
-    idx_jcn = findall(x -> occursin("jcn", String(Symbol(x))), ps)
-    idxs_other_params = setdiff(eachindex(ps), vcat(idxs_weight, idx_stim, idx_jcn))
-
-    env = ClassificationEnvironment(stim; name=:env, namespace=global_ns)
-    run_experiment!(agent, env; alg=Tsit5(), reltol=1e-6,abstol=1e-9)
 
     final_params = agent.problem.p
     # At least some weights need to be different.
