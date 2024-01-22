3ae896935d40728d5ab90672ca7adbafd2362f51
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-01-22 05:25:13 -0500

    Update docs/pages.jl

--- docs/pages.jl
+++ docs/pages.jl
@@ -1,7 +1,7 @@
 # Put in a separate page so it can be used by SciMLDocs.jl
 
 pages = ["index.md",
-    "getting_started.md",
+    #"getting_started.md",
     #"Tutorials" => Any[],
     #"Manual" => Any[],
     "API" => "api.md",
