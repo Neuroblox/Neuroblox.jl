47fa5ef105e7263917091000662b114a9f6df921
Helmut Strey <Helmut.Strey@stonybrook.edu>
2024-06-03 23:38:48 -0400

    fully functioning example

--- examples/whole_brain_2dosc.jl
+++ examples/whole_brain_2dosc.jl
@@ -7,6 +7,7 @@ using DifferentialEquations
 using Random
 using Plots
 using Statistics
+using HypothesisTests
 
 weights = CSV.read("examples/weights.csv",DataFrame)
 region_names = names(weights)
@@ -15,7 +16,7 @@ wm = Array(weights)
 
 blocks = []
 for i in 1:size(wm)[1]
-    push!(blocks, Neuroblox.Generic2dOscillator(name=Symbol(region_names[i])))
+    push!(blocks, Neuroblox.Generic2dOscillator(name=Symbol(region_names[i]),bn=sqrt(5e-4)))
 end
 
 g = MetaDiGraph()
@@ -30,6 +31,23 @@ prob = SDEProblem(sys,rand(-2:0.1:4,76*2), (0.0, 10*60e3), [])
 
 plot(sol.t,sol[5,:],xlims=(0,10000))
 
-solv = Array(sol[1:2:end,1000:2000])'
-cor_matrix = cor(solv)
-heatmap(atanh.(cor_matrix))
+solv = Array(sol[1:2:end,1:1000])'
+cs = []
+for i in 1:Int((length(sol.t)-1)/1000)-1
+    solv = Array(sol[1:2:end,(i-1)*1000+1:(i*1000)])'
+    push!(cs,cor(solv))
+end
+css = stack(cs)
+cssath = atanh.(css)
+for i in 1:size(cssath)[1]
+    cssath[i,i,:] = zeros(size(cssath)[3])
+end
+p = zeros(76,76)
+for i in 1:76
+    for j in 1:76
+        p[i,j] = pvalue(OneSampleTTest(css[i,j,:]))
+    end
+end
+p
+heatmap(-log10.(p) .* (p .< 0.05))
+heatmap(wm)
