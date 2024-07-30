63bfa4cf8363dd0098434f992d57326c6a391417
Haris Orgn <organtzh@gmail.com>
2024-07-30 18:07:38 +0300

    rename `cortical_blox.jl` to `cortical.jl` to include more cortical-like types in it

--- src/blox/cortical.jl
+++ src/blox/cortical.jl
@@ -68,3 +68,74 @@ struct CorticalBlox <: CompositeBlox
         new(namespace, vcat(wtas, n_ff_inh), sys, bc)
     end
 end
+
+struct LIFExciCircuitBlox <: CompositeBlox
+    namespace
+    parts
+    odesystem
+    connector
+
+    function LIFExciCircuitBlox(
+        name, 
+        N_neurons,
+        namespace=nothing,
+        g_L = 25 * 1e-3, # mS
+        V_L = -70, # mV
+        V_E = 0, # mV
+        V_I = -70, # mV
+        θ = -50, # mV
+        V_reset = -55, # mV
+        C = 0.5 * 1e-3, # mF 
+        τ_AMPA = 2, # ms
+        τ_GABA = 5, # ms
+        τ_NMDA_decay = 100, # ms
+        τ_NMDA_rise = 2, # ms
+        α = 0.5, # ms⁻¹
+        g_AMPA = 0.05 * 1e-3, # mS
+        g_AMPA_external = 2.1 * 1e-3, # mS
+        g_GABA = 1.3 * 1e-3, # mS
+        g_NMDA = 0.165 * 1e-3, # mS  
+        Mg = 1, # mM
+        kwargs...
+        )
+
+        neurons = map(Base.OneTo(N_neurons)) do i
+            LIFExciNeuron(;
+                name = Symbol("neuron$i"),
+                namespace = namespaced_name(namespace, name),
+                g_L,
+                V_L,
+                V_E,
+                V_I,
+                θ,
+                V_reset,
+                C,
+                τ_AMPA,
+                τ_GABA,
+                τ_NMDA_decay,
+                τ_NMDA_rise,
+                α,
+                g_AMPA,
+                g_AMPA_external,
+                g_GABA,
+                g_NMDA,
+                Mg
+            )
+        end
+
+        g = MetaDiGraph()
+        add_blox!.(Ref(g), neurons)
+
+        for i in eachindex(neurons)
+            for j in eachindex(neurons)
+                add_edge!(g, i, j, Dict(kwargs))
+            end
+        end
+
+        bc = connector_from_graph(g)
+        
+        sys = isnothing(namespace) ? system_from_graph(g, bc; name) : system_from_parts(vcat(wtas, n_ff_inh); name)
+
+        new(namespace, neurons, sys, bc)
+    end
+end
