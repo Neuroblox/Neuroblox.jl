fe8bce7eec48385b9e4bf697cc30dfc331c410fd
david-hofmann <david.hofmann@mytum.de>
2024-07-31 14:39:53 +0200

    fixed typing in VLSetup

--- src/datafitting/spectralDCM.jl
+++ src/datafitting/spectralDCM.jl
@@ -30,15 +30,14 @@ mutable struct VLState
     dFdθθ::Matrix{Float64}       # free energy Hessian w.r.t. parameters
 end
 
-struct VLSetup#{Model, N}
-    model_at_x0                        # model evaluated at initial conditions
-    y_csd::Array{ComplexF64}               # cross-spectral density approximated by fitting MARs to data
+struct VLSetup{Model, T1 <: Array{ComplexF64}, T2 <: AbstractArray}
+    model_at_x0::Model                        # model evaluated at initial conditions
+    y_csd::T1                                 # cross-spectral density approximated by fitting MARs to data
     tolerance::Float64                        # convergence criterion
     systemnums::Vector{Int}                   # several integers -> np: n. parameters, ny: n. datapoints, nq: n. Q matrices, nh: n. hyperparameters
     systemvecs::Vector{Vector{Float64}}       # μθ_pr: prior expectation values of parameters and μλ_pr: prior expectation values of hyperparameters
     systemmatrices::Vector{Matrix{Float64}}   # Πθ_pr: prior precision matrix of parameters, Πλ_pr: prior precision matrix of hyperparameters
-    # Q::Matrix{ComplexF64}                   # linear decomposition of precision matrix of parameters, typically just one matrix, the empirical correlation matrix
-    Q::Array{ComplexF64}                      # linear decomposition of precision matrix of parameters, typically just one matrix, the empirical correlation matrix
+    Q::T2                                     # linear decomposition of precision matrix of parameters, typically just one matrix, the empirical correlation matrix
 end
 
 """
