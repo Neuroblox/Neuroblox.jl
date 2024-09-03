4e56aa6b740bea1457bf7029e34e6c231a507a70
Mason Protter <mason.protter@icloud.com>
2024-09-03 15:02:59 -0400

    Store a bit more info in composite blox, tweak `get_` functions (#387)
    * fix merge conflict
    
    * switch to providing an explicit RNG for WTA
    
    * Make some tweaks for GraphSystems compatability
    
    * move change from cortical_blox.jl to cortical.jl
    
    * uncomment
    
    * store important tau/tau_s params from FSI blox
    
    * add methods for generating an indegree_constrained connection matrix
    
    * store event times

--- src/blox/blox_utilities.jl
+++ src/blox/blox_utilities.jl
@@ -168,61 +168,69 @@ get_weight_learning_rules(blox) = Dict{Num, AbstractLearningRule}()
 get_blox_parts(blox::Union{CompositeBlox, AbstractComponent}) = blox.parts
 
 function get_weight(kwargs, name_blox1, name_blox2)
-    if haskey(kwargs, :weight)
-        return kwargs[:weight]
-    else
+    get(kwargs, :weight) do
         @warn "Connection weight from $name_blox1 to $name_blox2 is not specified. Assuming weight=1" 
-        return 1
+        return 1.0
     end
 end
 
 function get_gap_weight(kwargs, name_blox1, name_blox2)
-    if haskey(kwargs, :gap_weight)
-        return kwargs[:gap_weight]
-    else
+    get(kwargs, :gap_weight) do
         error("Gap junction weight from $name_blox1 to $name_blox2 is not specified.")
     end
 end
 
 function get_weightmatrix(kwargs, name_blox1, name_blox2)
-    if haskey(kwargs, :weightmatrix)
-        return kwargs[:weightmatrix]
-    else
+    get(kwargs, :weightmatrix) do
         error("Connection weight from $name_blox1 to $name_blox2 is not specified.")
     end
 end
 
 function get_delay(kwargs, name_blox1, name_blox2)
-    if haskey(kwargs, :delay)
-        return kwargs[:delay]
-    else
-#        @warn "Delay constant from $name_blox1 to $name_blox2 is not specified. It is assumed that there is no delay."
+    get(kwargs, :delay) do 
+#        @debug "Delay constant from $name_blox1 to $name_blox2 is not specified. It is assumed that there is no delay."
         return 0
     end
 end
 
 function get_density(kwargs, name_blox1, name_blox2)
-    if haskey(kwargs, :density)
-        return kwargs[:density]
-    else 
+    get(kwargs, :density) do 
         error("Connection density from $name_blox1 to $name_blox2 is not specified.")
     end
 end
 
 function get_sta(kwargs, name_blox1, name_blox2)
-    haskey(kwargs, :sta) ? kwargs[:sta] : false    
+    get(kwargs, :sta, false)
 end
 
 function get_gap(kwargs, name_blox1, name_blox2)
-    haskey(kwargs, :gap) ? kwargs[:gap] : false    
+    get(kwargs, :gap, false)    
 end
 
 function get_event_time(kwargs, name_blox1, name_blox2)
-    if haskey(kwargs, :t_event)
-        return kwargs[:t_event]
-    else 
+    get(kwargs, :t_event) do
         error("Time for the event that affects the connection from $name_blox1 to $name_blox2 is not specified.")
     end
+end
+
+function get_connection_matrix(kwargs, name_out, name_in, N_out, N_in)
+    sz = (N_out, N_in)
+    connection_matrix = get(kwargs, :connection_matrix) do
+        density = get_density(kwargs, name_out, name_in)
+        dist = Bernoulli(density)
+        rng = get(kwargs, :rng, Random.default_rng())
+        rand(rng, dist, sz...)
+    end
+    if size(connection_matrix) != sz
+        error(ArgumentError("The supplied connection matrix between $(name_out) and $(name_in) is an "
+                            * "incorrect size. Got $(size(connection_matrix)), whereas $(name_out) has "
+                            * "$N_out excitatory neurons, and $name_in has $N_in excitatory neurons."))
+    end
+    if eltype(connection_matrix) != Bool
+        error(ArgumentError("The supplied connection matrix between $(name_out) and $(name_in) must "
+                            * "be an array of Bool, got $(eltype(connection_matrix)) instead."))
+    end
+    connection_matrix
 end
 
 function get_weights(agent::Agent, blox_out, blox_in)
@@ -339,9 +347,7 @@ function addnontunableparams(paramlist, sys)
 end
 
 function get_connection_rule(kwargs, bloxout, bloxin, w)
-    if haskey(kwargs, :connection_rule)
-        cr = kwargs[:connection_rule]
-    else
+    cr = get(kwargs, :connection_rule) do
         name_blox1 = nameof(bloxout)
         name_blox2 = nameof(bloxin)
         @warn "Neuron connection rule from $name_blox1 to $name_blox2 is not specified. It is assumed that there is a basic weighted connection."
--- src/blox/connections.jl
+++ src/blox/connections.jl
@@ -98,13 +98,31 @@ function hypergeometric_connections!(bc, neurons_out, neurons_in, name_out, name
     end
 end
 
-function indegree_constrained_connections!(bc, neurons_out, neurons_in, name_out, name_in; kwargs...)
-    density = get_density(kwargs, name_out, name_in)
-    in_degree =  Int(ceil(density * length(neurons_out)))
-    for neuron_postsyn in neurons_in
-        idx = sample(collect(1:length(neurons_out)), in_degree; replace=false)
-        for neuron_presyn in neurons_out[idx]
-            bc(neuron_presyn, neuron_postsyn; kwargs...)
+function indegree_constrained_connection_matrix(density, N_src, N_dst; kwargs...)
+    rng = get(kwargs, :rng, Random.default_rng())
+    in_degree =  Int(ceil(density * N_src))
+    conn_mat = falses(N_src, N_dst)
+    for j ∈ 1:N_dst
+        idx = sample(rng, 1:N_src, in_degree; replace=false)
+        for i ∈ idx
+            conn_mat[i, j] = true
+        end
+    end
+    conn_mat
+end
+
+function indegree_constrained_connections!(bc, neurons_src, neurons_dst, name_src, name_dst; kwargs...)
+    N_src = length(neurons_src)
+    N_dst = length(neurons_dst)
+    conn_mat = get(kwargs, :connection_matrix) do
+        density = get_density(kwargs, name_src, name_dst)
+        indegree_constrained_connection_matrix(density, N_src, N_dst; kwargs...)
+    end
+    for j ∈ 1:N_dst
+        for i ∈ 1:N_src
+            if conn_mat[i, j]
+                bc(neurons_src[i], neurons_dst[j]; kwargs...)
+            end
         end
     end
 end
@@ -133,7 +151,7 @@ function (bc::BloxConnector)(
     sys_in = get_namespaced_sys(HH_in)
 
     w = generate_weight_param(HH_out, HH_in; kwargs...)
-    push!(bc.weights, w)    
+    push!(bc.weights, w)
 
     if haskey(kwargs, :learning_rule)
         lr = deepcopy(kwargs[:learning_rule])
@@ -143,8 +161,6 @@ function (bc::BloxConnector)(
     end
 
     STA = get_sta(kwargs, nameof(HH_out), nameof(HH_in))
-
-    
     eq = if STA
         sys_in.I_syn ~ -w * sys_in.Gₛₜₚ * sys_out.G * (sys_in.V - sys_out.E_syn)
     else
@@ -152,17 +168,6 @@ function (bc::BloxConnector)(
     end
 
     accumulate_equation!(bc, eq)
-    
-    GAP = get_gap(kwargs, nameof(HH_out), nameof(HH_in))
-    if GAP
-        w_gap = generate_gap_weight_param(HH_out, HH_in; kwargs...)
-        push!(bc.weights, w_gap)
-        eq2 = sys_in.I_gap ~ -w_gap * (sys_in.V - sys_out.V)
-        accumulate_equation!(bc, eq2) 
-        eq3 = sys_out.I_gap ~ -w_gap * (sys_out.V - sys_in.V)
-        accumulate_equation!(bc, eq3) 
-    end
-
 end
 
 function (bc::BloxConnector)(
@@ -174,21 +179,11 @@ function (bc::BloxConnector)(
     sys_in = get_namespaced_sys(HH_in)
 
     w = generate_weight_param(HH_out, HH_in; kwargs...)
-    push!(bc.weights, w)    
+    push!(bc.weights, w)
 
     eq = sys_in.I_syn ~ -w * sys_out.G * (sys_in.V - sys_out.E_syn)
     
     accumulate_equation!(bc, eq)
-
-    GAP = get_gap(kwargs, nameof(HH_out), nameof(HH_in))
-    if GAP
-        w_gap = generate_gap_weight_param(HH_out, HH_in; kwargs...)
-        push!(bc.weights, w_gap)
-        eq2 = sys_in.I_gap ~ -w_gap * (sys_in.V - sys_out.V)
-        accumulate_equation!(bc, eq2) 
-        eq3 = sys_out.I_gap ~ -w_gap * (sys_out.V - sys_in.V)
-        accumulate_equation!(bc, eq3) 
-    end
 end
 
 function (bc::BloxConnector)(
@@ -202,9 +197,8 @@ function (bc::BloxConnector)(
     w = generate_weight_param(HH_out, HH_in; kwargs...)
     push!(bc.weights, w)    
 
-        
     eq = sys_in.I_syn ~ -w * sys_out.Gₛ * (sys_in.V - sys_out.E_syn)
-    
+
     accumulate_equation!(bc, eq)
 
     GAP = get_gap(kwargs, nameof(HH_out), nameof(HH_in))
@@ -488,20 +482,20 @@ end
 function (bc::BloxConnector)(
     wta_out::WinnerTakeAllBlox, 
     wta_in::WinnerTakeAllBlox; 
-    kwargs...
-)
-    neurons_in = get_exci_neurons(wta_in)
+    kwargs...)
     neurons_out = get_exci_neurons(wta_out)
-    rng = get(kwargs, :rng, Random.default_rng())
-    density = get_density(kwargs, nameof(wta_out), nameof(wta_in))
-    dist = Bernoulli(density)
-
-    for neuron_postsyn in neurons_in
+    neurons_in = get_exci_neurons(wta_in)
+    # users can supply a :connection_matrix to the graph edge, where
+    # connection_matrix[i, j] determines if neurons_out[i] is connected to neurons_out[j] 
+    connection_matrix = get_connection_matrix(kwargs,
+                                              namespaced_nameof(wta_out), namespaced_nameof(wta_in),
+                                              length(neurons_out), length(neurons_in))
+    for (j, neuron_postsyn) in enumerate(neurons_in)
         name_postsyn = namespaced_nameof(neuron_postsyn)
-        for neuron_presyn in neurons_out
+        for (i, neuron_presyn) in enumerate(neurons_out)
             name_presyn = namespaced_nameof(neuron_presyn)
             # Check names to avoid recurrent connections between the same neuron
-            if (name_postsyn != name_presyn) && rand(rng, dist)
+            if (name_postsyn != name_presyn) && connection_matrix[i, j]
                 bc(neuron_presyn, neuron_postsyn; kwargs...)
             end
         end
