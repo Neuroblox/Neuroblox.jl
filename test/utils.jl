7518b15551934db8693ed8f0828cd040d83f3a11
Haris Orgn <organtzh@gmail.com>
2024-09-04 20:32:02 +0300

    fix tests

--- test/utils.jl
+++ test/utils.jl
@@ -21,27 +21,29 @@ end
     global_ns = :g 
     tspan = (0, 200)
     V_reset = -55
-
+    
     @named s = PoissonSpikeTrain(3, tspan; namespace = global_ns)
     @named n = LIFExciCircuitBlox(; V_reset, namespace = global_ns, N_neurons = 3, weight=1)
-
+    
     neurons = [s, n]
-
+    
     g = MetaDiGraph()
     add_blox!.(Ref(g), neurons)
-
+    
     add_edge!(g, 1, 2, Dict(:weight => 1))
-
+    
     sys = system_from_graph(g; name = global_ns)
     ss = structural_simplify(sys)
     prob = ODEProblem(ss, [], (0, 200.0))
     sol = solve(prob, Tsit5())
-
+    
     V = hcat(sol[ss.n.neuron1.V], sol[ss.n.neuron2.V], sol[ss.n.neuron3.V])
     V[V .== V_reset] .= NaN
-
-    @test all(isequal(V, Neuroblox.voltage_timeseries(n, sol)))
-
+    
+    V_nb = Neuroblox.voltage_timeseries(n, sol)
+    Neuroblox.replace_refractory!(V_nb, n, sol)
+    @test all(isequal(V, V_nb))
+    
     V_filtered = map(eachrow(V)) do V_t
         v = filter(!isnan, V_t)
         mean(v)
@@ -49,3 +51,4 @@ end
     
     @test all(isequal(V_filtered, Neuroblox.meanfield_timeseries(n, sol)))
 end
+
