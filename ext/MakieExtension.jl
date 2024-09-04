0bad72f87dfb45789d450a5fce6b0236d607932b
Haris Orgn <organtzh@gmail.com>
2024-09-04 18:05:17 +0300

    add Makie extension

---
+++ ext/MakieExtension.jl
@@ -0,0 +1,56 @@
+module MakieExtension
+
+isdefined(Base, :get_extension) ? using Makie : using ..Makie
+
+using Neuroblox
+using Neuroblox: CompositeBlox, meanfield_timeseries, detect_spikes, get_neurons
+using SciMLBase: AbstractSolution
+
+import Neuroblox: meanfield, meanfield!, rasterplot, rasterplot!
+
+function Makie.convert_arguments(::Makie.PointBased, cb::CompositeBlox, sol::AbstractSolution)
+    V = meanfield_timeseries(cb, sol)
+    
+    return (sol.t, vec(V))
+end
+
+@recipe(MeanField, blox, sol) do scene
+    Theme()
+end
+
+argument_names(::Type{<: MeanField}) = (:blox, :sol)
+
+function Makie.plot!(p::MeanField)
+    sol = p.sol[]
+    blox = p.blox[]
+
+    V = meanfield_timeseries(blox, sol)
+    
+    lines!(p, sol.t, vec(V))
+
+    return p
+end
+
+@recipe(RasterPlot, blox, sol) do scene
+    Theme(
+        color = :black
+    )
+end
+
+argument_names(::Type{<: RasterPlot}) = (:blox, :sol)
+
+function Makie.plot!(p::RasterPlot)
+    sol = p.sol[]
+    t = sol.t
+    blox = p.blox[]
+    neurons = get_neurons(blox)
+    
+    for (i, n) in enumerate(neurons)
+        spike_idxs = detect_spikes(n, sol)
+        scatter!(p, t[spike_idxs], fill(i, length(spike_idxs)); color=p.color[])
+    end
+    
+    return p
+end
+
+end
