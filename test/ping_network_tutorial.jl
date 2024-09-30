92029ee0329c6129dfe0fcdb0fcb217a7fbc1fa0
agchesebro <76024790+agchesebro@users.noreply.github.com>
2024-09-30 15:41:44 -0400

    Ok it mostly does the figure
    Had to tweak some parameters to get the dynamics working but what else is new in papers not replicating exactly

--- test/ping_network_tutorial.jl
+++ test/ping_network_tutorial.jl
@@ -3,21 +3,28 @@ using Neuroblox, DifferentialEquations, Distributions, Graphs, MetaGraphs, Rando
 # First focus is on producing panels from Figure 1 of the PING network paper.
 
 # Setup parameters from the supplemental material
-μ_E = 1.5
+μ_E = 0.8
 σ_E = 0.15
 μ_I = 0.8
 σ_I = 0.08
 
 # Define the PING network neuron numbers
-NE_driven = 2
-NE_other = 14
-NI_driven = 4
+NE_driven = 40
+NE_other = 120
+NI_driven = 40
 N_total = NE_driven + NE_other + NI_driven
 
+# Define the extra currents
+I_driveE = Normal(μ_E, σ_E)
+I_driveI = Normal(μ_I, σ_I)
+I_base = Normal(0, 0.1)
+I_undriven = Normal(0, 0.4)
+I_bath = -0.7
+
 # First, create the 20 driven excitatory neurons
-exci_driven = [PINGNeuronExci(name=Symbol("ED$i"), I_ext=rand(Normal(μ_I, σ_I))) for i in 1:NE_driven]
-exci_other  = [PINGNeuronExci(name=Symbol("EO$i")) for i in 1:NE_other]
-inhib       = [PINGNeuronInhib(name=Symbol("ID$i"), I_ext=rand(Normal(μ_I, σ_I))) for i in 1:NI_driven]
+exci_driven = [PINGNeuronExci(name=Symbol("ED$i"), I_ext=rand(I_driveE)+rand(I_base)) for i in 1:NE_driven]
+exci_other  = [PINGNeuronExci(name=Symbol("EO$i"), I_ext=rand(I_base) + rand(I_undriven)) for i in 1:NE_other]
+inhib       = [PINGNeuronInhib(name=Symbol("ID$i"), I_ext=rand(I_driveI) + rand(I_base)+I_bath) for i in 1:NI_driven]
 
 # Create the network
 g = MetaDiGraph()
@@ -27,7 +34,7 @@ add_blox!.(Ref(g), vcat(exci_driven, exci_other, inhib))
 N=N_total
 g_II=0.2 
 g_IE=0.6 
-g_EI=0.6
+g_EI=0.8
 
 for i = 1:NE_driven+NE_other
     for j = NE_driven+NE_other+1:N_total
@@ -43,15 +50,17 @@ for i = NE_driven+NE_other+1:N_total
 end
 
 @named sys = system_from_graph(g, graphdynamics=true)
-prob = ODEProblem(sys, [], (0.0, 20.0))
+prob = ODEProblem(sys, [], (0.0, 300.0))
 @time sol = solve(prob, Tsit5(), saveat=0.1)
 
-using Peaks, Plots
+# using Peaks, Plots
 
-exci_voltages = reduce(hcat, ModelingToolkit.getu(sol, vcat([Symbol("ED$i"*"₊V") for i in 1:NE_driven], [Symbol("EO$i"*"₊V") for i in 1:NE_other]))(sol))'
-inhib_voltages = reduce(hcat, ModelingToolkit.getu(sol, [Symbol("ID$i"*"₊V") for i in 1:NI_driven])(sol))'
+# exci_voltages = reduce(hcat, ModelingToolkit.getu(sol, vcat([Symbol("ED$i"*"₊V") for i in 1:NE_driven], [Symbol("EO$i"*"₊V") for i in 1:NE_other]))(sol))'
+# inhib_voltages = reduce(hcat, ModelingToolkit.getu(sol, [Symbol("ID$i"*"₊V") for i in 1:NI_driven])(sol))'
 
-plot(exci_voltages)
-plot(inhib_voltages)
+# Plots.plot(exci_voltages)
+# Plots.plot(inhib_voltages)
 
-maxima(inhib_voltages[:, 1])
+using CairoMakie
+rasterplot(vcat(exci_driven, exci_other), sol; threshold=20.0)
+rasterplot(inhib, sol; threshold=20.0)
