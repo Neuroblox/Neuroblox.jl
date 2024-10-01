af1923398a28c7014afbd82218025c8e68ac9b58
agchesebro <76024790+agchesebro@users.noreply.github.com>
2024-10-01 08:30:50 -0400

    Oops typo

--- docs/src/tutorials/ping_network_tutorial.jl
+++ docs/src/tutorials/ping_network_tutorial.jl
@@ -96,7 +96,7 @@ end
 # ## Simulate the network
 # Now that we have the neurons and the graph, we can simulate the network. We use the `system_from_graph` function to create a system of ODEs from the graph and then solve it using the DifferentialEquations.jl package.
 
-t_span = (0.0, 300.0) ## Time span for the simulation - run for 300ms to match the Börgers et al. [1] Figure 1.
+tspan = (0.0, 300.0) ## Time span for the simulation - run for 300ms to match the Börgers et al. [1] Figure 1.
 @named sys = system_from_graph(g, graphdynamics=true) ## Use GraphDynamics.jl otherwise this can be a very slow simulation
 prob = ODEProblem(sys, [], tspan) ## Create the problem to solve
 sol = solve(prob, Tsit5(), saveat=0.1) ## Solve the problem and save at 0.1ms resolution.
