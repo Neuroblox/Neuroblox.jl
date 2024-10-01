e11d45cdcde5faa12ed303c63dc99d3f08f29c16
agchesebro <76024790+agchesebro@users.noreply.github.com>
2024-10-01 08:16:03 -0400

    Fix paths and illustration locations

--- docs/src/tutorials/ping_network_tutorial.jl
+++ docs/src/tutorials/ping_network_tutorial.jl
@@ -16,7 +16,7 @@
 # ```
 # For full details of the model, see Eq. 12-14 on p. 7 of the SI Appendix of Börgers et al. [1].
 # Here is a visual representation of the network structure and which neurons receive the driving input:
-# ![PING network structure](/Users/achesebro/Documents/GitHub/Neuroblox.jl/docs/src/tutorials/ping_tutorial/basic_ping_illustration.png)
+# ![PING network structure](../assets/basic_ping_illustration.png)
 
 # # Model setup
 # This section sets up the model parameters and the network structure. The network consists of 200 neurons: 40 driven excitatory neurons, 120 other excitatory neurons, and 40 inhibitory neurons.
@@ -109,10 +109,10 @@ rasterplot(exci,  sol; threshold=20.0)
 rasterplot(inhib, sol; threshold=20.0)
 
 # The first command should show the dynamics in Figure 1.C, with a clear population of excitatory neurons firing together from the external driving current, and the other excitatory neurons exhibiting more stochastic bursts, as in:
-# ![Figure 1.C from Börgers et al.](docs/src/tutorials/ping_tutorial/ping_network_exci.png)
+# ![Figure 1.C from Börgers et al.](../assets/ping_network_exci.png)
 
 # The second command should show the dynamics in Figure 1.A, with the inhibitory neurons firing in a more synchronous manner than the excitatory neurons, as in:
-# ![Figure 1.A from Börgers et al.](docs/src/tutorials/ping_tutorial/ping_network_inhib.png)
+# ![Figure 1.A from Börgers et al.](../assets/ping_network_inhib.png)
 
 # # Conclusion
 # And there you have it! A complete PING demonstration that reproduces the dynamics of a published paper in a matter of 30 seconds, give or take. Have fun making your own!
