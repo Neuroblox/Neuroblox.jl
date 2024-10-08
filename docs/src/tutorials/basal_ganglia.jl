83d01c6632ad8406e5f49b3af80e7f8d11845668
gabrevaya <gabrevaya@df.uba.ar>
2024-10-08 11:50:30 -0300

    update `add_edge!` dispatch

--- docs/src/tutorials/basal_ganglia.jl
+++ docs/src/tutorials/basal_ganglia.jl
@@ -90,15 +90,12 @@ N_FSI = 50 ## number of Fast Spiking Interneurons
 @named msn = Striatum_MSN_Adam(namespace=global_ns, N_inhib = N_MSN)
 @named fsi = Striatum_FSI_Adam(namespace=global_ns, N_inhib = N_FSI)
 
-assembly = [msn, fsi]
-
 ḡ_FSI_MSN = 0.6 ## maximal conductance for FSI to MSN synapses [mS/cm^-2]
 density_FSI_MSN = 0.15 ## fraction of FSIs connecting to the MSN population
 weight_FSI_MSN = ḡ_FSI_MSN / (N_FSI * density_FSI_MSN) ## normalized synaptic weight
 
 g = MetaDiGraph()
-add_blox!.(Ref(g), assembly)
-add_edge!(g, 2, 1, Dict(:weight => weight_FSI_MSN, :density => density_FSI_MSN))
+add_edge!(g, fsi => msn; weight = weight_FSI_MSN, density = density_FSI_MSN)
 
 @named sys = system_from_graph(g)
 prob = SDEProblem(sys, [], tspan, [])
@@ -145,8 +142,6 @@ N_STN = 40 ## number of STN neurons
 @named gpe = GPe_Adam(namespace=global_ns, N_inhib = N_GPe)
 @named stn = STN_Adam(namespace=global_ns, N_exci = N_STN)
 
-assembly = [msn, fsi, gpe, stn]
-
 ḡ_MSN_GPe = 2.5 ## maximal conductance for MSN to GPe synapses [mS/cm^-2]
 ḡ_GPe_STN = 0.3 ## maximal conductance for GPe to STN synapses [mS/cm^-2]
 ḡ_STN_FSI = 0.165 ## maximal conductance for STN to FSI synapses [mS/cm^-2]
@@ -160,11 +155,10 @@ weight_GPe_STN = ḡ_GPe_STN / (N_GPe * density_GPe_STN)
 weight_STN_FSI = ḡ_STN_FSI / (N_STN * density_STN_FSI)
 
 g = MetaDiGraph()
-add_blox!.(Ref(g), assembly)
-add_edge!(g, 1, 3, Dict(:weight => weight_MSN_GPe, :density => density_MSN_GPe))
-add_edge!(g, 2, 1, Dict(:weight => weight_FSI_MSN, :density => density_FSI_MSN))
-add_edge!(g, 3, 4, Dict(:weight => weight_GPe_STN, :density => density_GPe_STN))
-add_edge!(g, 4, 2, Dict(:weight => weight_STN_FSI, :density => density_STN_FSI))
+add_edge!(g, fsi => msn, weight = weight_FSI_MSN, density = density_FSI_MSN)
+add_edge!(g, msn => gpe, weight = weight_MSN_GPe, density = density_MSN_GPe)
+add_edge!(g, gpe => stn, weight = weight_GPe_STN, density = density_GPe_STN)
+add_edge!(g, stn => fsi, weight = weight_STN_FSI, density = density_STN_FSI)
 
 @named sys = system_from_graph(g)
 prob = SDEProblem(sys, [], tspan, [])
@@ -232,17 +226,14 @@ fig
 @named msn = Striatum_MSN_Adam(namespace=global_ns, N_inhib = N_MSN, I_bg = 1.2519*ones(N_MSN), G_M = 1.2)
 @named fsi = Striatum_FSI_Adam(namespace=global_ns, N_inhib = N_FSI, I_bg = 4.511*ones(N_FSI), weight = 0.2, g_weight = 0.075)
 
-assembly = [msn, fsi, gpe, stn]
-
 ḡ_FSI_MSN = 0.48 ## decreased maximal conductance of FSI-MSN projection [mS/cm^-2]
 weight_FSI_MSN = ḡ_FSI_MSN / (N_FSI * density_FSI_MSN) ## normalized synaptic weight
 
 g = MetaDiGraph()
-add_blox!.(Ref(g), assembly)
-add_edge!(g, 2, 1, Dict(:weight => weight_FSI_MSN, :density => density_FSI_MSN))
-add_edge!(g, 1, 3, Dict(:weight => weight_MSN_GPe, :density => density_MSN_GPe))
-add_edge!(g, 3, 4, Dict(:weight => weight_GPe_STN, :density => density_GPe_STN))
-add_edge!(g, 4, 2, Dict(:weight => weight_STN_FSI, :density => density_STN_FSI))
+add_edge!(g, fsi => msn, weight = weight_FSI_MSN, density = density_FSI_MSN)
+add_edge!(g, msn => gpe, weight = weight_MSN_GPe, density = density_MSN_GPe)
+add_edge!(g, gpe => stn, weight = weight_GPe_STN, density = density_GPe_STN)
+add_edge!(g, stn => fsi, weight = weight_STN_FSI, density = density_STN_FSI)
 
 @named sys = system_from_graph(g)
 
