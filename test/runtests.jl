05b3191fc3cbf22fb98f9d92d8f3ded231482aab
Mason Protter <mason.protter@icloud.com>
2024-10-08 13:16:34 +0200

    Split test suite into separate groups which run on different CI processes (#442)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,12 @@
 using SafeTestsets
 
-@time @safetestset "Utilities" begin include("utils.jl") end
-@time @safetestset "Components Tests" begin include("components.jl") end
-@time @safetestset "Neurograph Tests" begin include("graphs.jl") end
+const GROUP = get(ENV, "GROUP", "All")
+
+if GROUP == "All" || GROUP == "Basics"
+    @time @safetestset "Utilities" begin include("utils.jl") end
+    @time @safetestset "Components Tests" begin include("components.jl") end
+    @time @safetestset "Neurograph Tests" begin include("graphs.jl") end
+end
 
 # Commenting out because we no longer use ODEfromGraph for neural masses
 # @time @safetestset "ODE from Graph Tests" begin include("ode_from_graph.jl") end
@@ -10,15 +14,20 @@ using SafeTestsets
 # @time @safetestset "Neural Signal Measurement Models Tests" begin include("measurementmodels.jl") end
 # @time @safetestset "Spectral Utilities Tests" begin include("spectraltools.jl") end
 
-@time @safetestset "Data Fitting Tests" begin include("datafitting.jl") end 
-#@time @safetestset "ODE from Graph and simulate" begin include("graph_to_dataframe.jl") end #This no longer works because it's built on the old blocks
-@time @safetestset "Learning Tests" begin include("learning.jl") end
-@time @safetestset "Control Tests" begin include("controllers.jl") end
-@time @safetestset "Source Tests" begin include("source_components.jl") end
-@time @safetestset "Reinforcement Learning Tests" begin include("reinforcement_learning.jl") end
-@time @safetestset "Cort-Cort plasticity Tests" begin include("plasticity.jl") end
+if GROUP == "All" || GROUP == "Advanced"
+    @time @safetestset "Data Fitting Tests" begin include("datafitting.jl") end 
+    #@time @safetestset "ODE from Graph and simulate" begin include("graph_to_dataframe.jl") end #This no longer works because it's built on the old blocks
+    @time @safetestset "Learning Tests" begin include("learning.jl") end
+    @time @safetestset "Control Tests" begin include("controllers.jl") end
+    @time @safetestset "Source Tests" begin include("source_components.jl") end
+    @time @safetestset "Reinforcement Learning Tests" begin include("reinforcement_learning.jl") end
+    @time @safetestset "Cort-Cort plasticity Tests" begin include("plasticity.jl") end
+end
 
-@time @safetestset "GraphDynamics vs MTK tests" begin include("GraphDynamicsTests/runtests.jl") end
+if GROUP == "All" || GROUP == "GraphDynamics"
+    @time @safetestset "GraphDynamics vs MTK tests" begin include("GraphDynamicsTests/runtests.jl") end
+end
+
 # fitting tests should be at the end since they take the longest
 # removing them for now until we have real fitting tests
 # @time @safetestset "Fitting Tests" begin include("fitting.jl") end
