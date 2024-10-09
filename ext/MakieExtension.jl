f72e12f7ddd833b583e3b7c22a9dfe58101984a7
Germán Abrevaya <gabrevaya@df.uba.ar>
2024-10-09 03:39:57 -0300

    Fine-tuning of basal ganglia tutorial for its correct display (#445)
    * - add compat for CairoMakie in the docs Project
    - change the syntax of title kwarg in rasterplots
    
    * fix image path in DCM tutorial
    
    * move the `CairoMakie` bound to Neuroblox Project.toml from docs
    
    * change `rasterplot` recipe to allow both syntaxes for setting the title
    
    * update `add_edge!` dispatch
    
    * change title syntax in `powerspectrumplot`
    
    * Minor edits of spectralDCM.jl
    
    Hijacking this PR to fix minor printing issue in spectral DCM tutorial. Thank you :)
    
    * Fix prior edits of spectralDCM.jl tutorial.
    
    Whoops! Fixed the prior fix. Now all should be good.
    
    ---------
    
    Co-authored-by: harisorgn <organtzh@gmail.com>
    Co-authored-by: David Hofmann <1681922+david-hofmann@users.noreply.github.com>

--- ext/MakieExtension.jl
+++ ext/MakieExtension.jl
@@ -109,8 +109,10 @@ function Makie.plot!(p::RasterPlot)
     ax = current_axis()
     ax.xlabel = p.Axis.xlabel[]
     ax.ylabel = p.Axis.ylabel[]
-    ax.title = p.title[]
-
+    if p.title[] != ""
+        ax.title = p.title[]
+    end
+    
     spikes = detect_spikes(blox, sol; threshold=threshold)
     spike_times, neuron_indices = findnz(spikes)
     scatter!(p, sol.t[spike_times], neuron_indices; color=p.color[])
@@ -233,6 +235,7 @@ end
             xticks = [8,12,20,30, 40, 50,60,70,80,90],
             yscale = log10,
         ),
+        title = "",
         xlims = (8, 100),
         ylims = (1e-3, 10),
         alpha_start = 8,
@@ -263,6 +266,9 @@ function Makie.plot!(p::PowerSpectrumPlot)
     ax.ylabel = p.Axis.ylabel[]
     ax.xticks = p.Axis.xticks[]
     ax.yscale = p.Axis.yscale[]
+    if p.title[] != ""
+        ax.title = p.title[]
+    end
 
     if p.show_bands[]
         y1 = p.ylims[][1]
