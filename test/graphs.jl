1c4d99500c297f97b0477cdfea8a9501f805d81d
haris organtzidis <organtzh@gmail.com>
2024-10-29 14:26:45 +0200

    Add tests for AdjacencyMatrix (#477)
    * add tests for AdjacencyMatrix
    
    * fix import and typo
    
    * fix names test

--- test/graphs.jl
+++ test/graphs.jl
@@ -1,8 +1,65 @@
 using Neuroblox
-using Test
-using SparseArrays
+using Neuroblox: get_adjacency
 using Graphs
 using MetaGraphs
+using Test
+using SparseArrays
+
+@testset "AdjacencyMatrix [HH Neurons]" begin
+    @named n1 = HHNeuronExciBlox()
+    @named n2 = HHNeuronExciBlox()
+    @named n3 = HHNeuronInhibBlox()
+
+    g = MetaDiGraph()
+    add_edge!(g, n1 => n2 , weight = 1)
+    add_edge!(g, n1 => n3 , weight = 1)
+    add_edge!(g, n3 => n2 , weight = 1)
+    add_edge!(g, n2 => n2 , weight = 1)
+
+    adj = get_adjacency(g) 
+
+    A = [0 1 1 ; 0 1 0; 0 1 0]
+
+    @test all(A .== adj.matrix)
+    @test all([:n1, :n2, :n3] .== adj.names)
+end
+
+@testset "AdjacencyMatrix [CorticalBlox]" begin
+    global_ns = :g
+
+    A = Matrix{Matrix{Bool}}(undef, 2, 2)
+    A[2,1] = [0 1 ; 1 1]
+    A[1,2] = [0 1 ; 1 1]
+
+    @named cb1 = CorticalBlox(namespace = global_ns, N_wta=2, N_exci=2, connection_matrices=A, weight=1)
+
+    adj = get_adjacency(cb1) 
+
+    adj_wta_11 = [0 1 1; 1 0 0; 1 0 0]
+    adj_wta_12 = [[0 0 0]; hcat([0, 0], A[1,2])]
+    adj_wta_21 = [[0 0 0]; hcat([0, 0], A[2,1])]
+
+    A_wta = [adj_wta_11 adj_wta_12 ; adj_wta_21 adj_wta_11]
+
+    A = [
+        hcat(A_wta, [0, 0, 0, 0, 0, 0]);
+        [0 1 1 0 1 1 0]
+    ]
+
+    @test all(A .== adj.matrix)
+
+    nms = [
+        :cb1₊wta1₊inh,
+        :cb1₊wta1₊exci1,
+        :cb1₊wta1₊exci2,
+        :cb1₊wta2₊inh,
+        :cb1₊wta2₊exci1,
+        :cb1₊wta2₊exci2,
+        :cb1₊ff_inh
+    ]
+
+    @test all(nms .== adj.names)
+end
 
 @testset "Graph to adjacency matrix" begin
     # testing whether creating a simple graph results in the correct adjacency matrix
