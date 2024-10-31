9c534e39578cc5de576a6022f690d760ed2bd2dd
agchesebro <76024790+agchesebro@users.noreply.github.com>
2024-10-31 18:50:20 -0400

    Overhaul of Parkinson's JR tutorial
    This has some pretty significant changes that should address nearly all the points

--- docs/src/tutorials/parkinsons.jl
+++ docs/src/tutorials/parkinsons.jl
@@ -6,7 +6,9 @@
 # - How to create a Jansen-Rit neural mass model in Neuroblox
 # - How to create blocks with different default sets of parameters
 # - Using symbolic hyperparameters during graph definition
-# - Visualizing neural mass outputs in both temporal and spectral plots 
+# - Extracting specific solutions states for plotting time series data
+# - Remaking an ``ODEProblem`` without needing to recompile an entire ``ODESystem``
+# - Plotting basic spectrograms of simulations
 
 # ## The Jansen-Rit Neural Mass Model
 
@@ -73,17 +75,14 @@ using CairoMakie
 # ## Building the Circuit
 
 # Now, let's create a graph representing our brain circuit. The nodes on this graph are the neural mass models defined aboe and the edges are the connections between the nodes based on the known anatomy of the basal ganglia-thalamocortical circuit.
-# We define the connections specified in Table 2 of Liu et al. [1], although we only implement a subset of the nodes and edges to describe a simplified version of the model.
-# Our connections share some common parameters which we define here, both as symbols and values, and use them as expressions for the weight of each connection :
-
+# We define the connection weights as specified in Table 2 of [1], and the signs (excitatory vs inhibitory) based on the connections in Figure 1.
+# Note that Julia can use any unicode character as a variable name, so we can use arrows in the names of the weights. If you're typing these from scratch, you can access these by typing ``\rightarrow``. For more examples, check out the [Julia documentation](https://docs.julialang.org/en/v1/manual/unicode-input/).
+# We also use the ``@parameters`` macro to define the common connection parameters, which we can then use to define the connections. This is a macro from [ModelingToolkit](https://docs.sciml.ai/ModelingToolkit/stable/tutorials/ode_modeling/) in Julia, which you should look at if you want to build more general models.
 g = MetaDiGraph() ## define an empty graph
 
-params = @parameters C_Cor=60 C_BGTh=60 C_Cor➡BGTh=5 C_BGTh➡Cor=5 # define common connection parameters
-## additional paramters for healthy version
-##params = @parameters C_Cor=3 C_BGTh=3 C_Cor➡BGTh=9.75 C_BGTh➡Cor=9.75 # define common connection parameters
+params = @parameters C_Cor=3 C_BGTh=3 C_Cor➡BGTh=9.75 C_BGTh➡Cor=9.75 ## define common connection parameters using healthy parameter range
 
 ## Create connections
-# Values come from Table 2, signs come from Figure 1
 
 ## thalamocortical connection
 add_edge!(g, Th => EI; weight = C_BGTh➡Cor)
@@ -117,24 +116,62 @@ add_edge!(g, PY => EI; weight = 6*C_Cor)
 add_edge!(g, PY => II; weight = 1.5*C_Cor)
 add_edge!(g, EI => PY; weight = 4.8*C_Cor)
 add_edge!(g, II => PY; weight = -1.5*C_Cor)
-add_edge!(g, II => II; weight = -3.3*C_Cor)
+add_edge!(g, II => II; weight = -3.3*C_Cor);
 
 # ## Creating the Model
 
 # Let's build the complete model:
 
-@named final_system = system_from_graph(g)
+@named final_system = system_from_graph(g);
 
 # This creates a differential equations system from our graph representation using ModelingToolkit and symbolically simplifies it for efficient computation.
 
 # ## Simulating the Model
 
-# Lastly, we create the `ODEProblem` for our system, select an algorithm, in this case `Tsit5()`, and simulate 1 second of brain activity.
+# Lastly, we create the ``ODEProblem``` for our system, select an algorithm, in this case ``Tsit5()`` (see discussion in the previous tutorial about solver choices if you're interested), and simulate 1 second of brain activity.
 
-sim_dur = 1000.0 # Simulate for 1 second
-prob = ODEProblem(final_system, [], (0.0, sim_dur))
-sol = solve(prob, Tsit5(), saveat=1)
+sim_dur = 1000.0 ## Simulate for 1 second
+prob = ODEProblem(final_system, [], (0.0, sim_dur)) ## Create the problem to solve
+sol = solve(prob, Tsit5(), saveat=0.1); ## Solve the problem and save every 0.1ms
 
+# ## Visualizing the results
+# Let's interrogate the solution to see what we have. For the purposes of this tutorial, we'll focus on the striatal oscillations.
+# In this simple model, we should see relatively sharp on/off transitions in the striatal populations. To test this, let's use [Symbolic Indexing](https://docs.sciml.ai/SymbolicIndexingInterface/stable/usage/) to access the states we're interested in: the ``y`` state of the D1 neuron population.
+
+idx_func = ModelingToolkit.getu(sol, D1.odesystem.y); ## gets the state index of the D1 neuron population in the solution object
+
+# Now use this indexing function to plot the solution in a Makie plot ([read more about Makie in the docs](https://docs.makie.org/stable/tutorials/getting-started)).
+
+fig = Figure() ## create the figure 
+ax = Axis(fig[1, 1], xlabel="Time (ms)", ylabel="y voltage (arbitrary units)", title="D1 population membrane voltage") ## define one axis for the figure
+lines!(ax, sol.t, idx_func(sol), color = :blue) ## plot the time series of the D1 neuron population
+display(fig) ## display the figure
+
+# Great! The striatum is oscillating. A Parkinson's model is better evaluated in the frequency domain, so let's lengthen the simulation now and plot a spectrogram.
+# To do this, we'll use the ``remake`` function to create a new ``ODEProblem`` with a longer simulation time without rebuilding the entire system.
+
+sim_dur = 10000.0 ## Simulate for 10 seconds
+prob = remake(prob, tspan = (0.0, sim_dur)) ## Remake the ODEProblem with the new simulation time
+sol = solve(prob, Tsit5(), saveat=0.1); ## Solve the new problem
+
+# Let's use this new solution to plot the power spectrum of the D1 neuron population. This is using a built-in Neuroblox function ``powerspectrumplot`` that's discussed at length in the next tutorial, so we won't delve into the details of the arguments here.
+
+powerspectrumplot(D1, sol, state = "y", ## specify the block to plot, the solution object, and the state of the block to plot
+                  method = welch_pgram, window = hanning, ylims = (1e-6, 1e6), ## additional parameters to make the plot pretty
+                  alpha_label_position = (8.5, 1e5),
+                  beta_label_position = (22, 1e5),
+                  gamma_label_position = (60, 1e5),
+                  title="D1 Population Power Spectrum")
+
+# You should see two prominent β-band peaks: one in low β (around 15 Hz) and one in high β (around 35 Hz). You should also see their resonances in the γ-band.
+
+# ## Further Exploration
+# You might be wondering: what about Parkinson's in this model? Well, like all good things, that is left as an exercise for the reader. Following the original paper, try replacing the original parameters with Parkinson's versions, so that 
+# ``C_Cor=60``, ``C_BGTh=60``, ``C_Cor➡BGTh=5``, and ``C_BGTh➡Cor=5``. If you just change these values, you should see the D1 population have a dramatic increase in β power at the lower frequency peak and a complete loss of the higher frequency peak.
+# We've selected the plotting parameters so you can use the same plotting call above to see this difference - so no excuse not to run this on your own!
+# If you're inspired to continue experimenting, here are a couple other ideas:
+# - Try looking at the ``PY`` population under normal and Parkinson's conditions. Hint: you'll need to adjust the ``ylims`` of the power spectrum to fully appreciate the changes.
+# - Try changing the lengths of the simulation and the sampling frequency. How does this affect the power spectrum?
 
 # ## References
 # [1] Liu, C, Zhou, C, Wang, J, Fietkiewicz, C, & Loparo, KA. (2020). The role of coupling connections in a model of the cortico-basal ganglia-thalamocortical neural loop for the generation of beta oscillations. Neural Networks, 123, 381-392. DOI: [10.1016/j.neunet.2019.12.021](https://doi.org/10.1016/j.neunet.2019.12.021)
