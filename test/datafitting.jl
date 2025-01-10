fb3ee653a7f0ae47e7c547f2d00246bd5df5cffc
Mason Protter <mason.protter@icloud.com>
2025-01-10 20:29:06 +0100

    don't use `with_stack` on the DCM test

--- test/datafitting.jl
+++ test/datafitting.jl
@@ -236,21 +236,16 @@ end
     csdsetup = (mar_order = 8, freq = freq, dt = dt);
 
     (state, setup) = setup_sDCM(data, fullmodel, initcond, csdsetup, priors, hyperpriors, indices, modelparam, "LFP");
-    # HACK: on machines with very small amounts of RAM, Julia can run out of stack space while compiling the code called in this loop
-    # this should be rewritten to abuse the compiler less, but for now, an easy solution is just to run it with more allocated stack space.
-    with_stack(f, n) = fetch(schedule(Task(f,n)))
 
-    with_stack(5_000_000) do  # 5MB of stack space
-        for iter in 1:128
-            state.iter = iter
-            run_sDCM_iteration!(state, setup)
-            print("iteration: ", iter, " - F:", state.F[end] - state.F[2], " - dF predicted:", state.dF[end], "\n")
-            if iter >= 4
-                criterion = state.dF[end-3:end] .< setup.tolerance
-                if all(criterion)
-                    print("convergence\n")
-                    break
-                end
+    for iter in 1:128
+        state.iter = iter
+        run_sDCM_iteration!(state, setup)
+        print("iteration: ", iter, " - F:", state.F[end] - state.F[2], " - dF predicted:", state.dF[end], "\n")
+        if iter >= 4
+            criterion = state.dF[end-3:end] .< setup.tolerance
+            if all(criterion)
+                print("convergence\n")
+                break
             end
         end
     end
