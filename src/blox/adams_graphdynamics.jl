fd0e4bc685bfcacc5352de08f0163ee3240a85e7
Haris Orgn <organtzh@gmail.com>
2025-03-25 11:20:57 +0200

    add `Glu` dynamics to the `AdamNMDAR` blox

--- src/blox/adams_graphdynamics.jl
+++ src/blox/adams_graphdynamics.jl
@@ -144,7 +144,6 @@ function GraphDynamicsInterop.subsystem_differential(s::Subsystem{AdamGABBA}, in
     )
 end
 
-
 GraphDynamicsInterop.issupported(::AdamNMDAR) = true
 GraphDynamicsInterop.components(v::AdamNMDAR) = (v,)
 
@@ -158,8 +157,11 @@ function GraphDynamicsInterop.to_subsystem(v::AdamNMDAR)
     k_block = GraphDynamicsInterop.recursive_getdefault(v.system.k_block)
     α = GraphDynamicsInterop.recursive_getdefault(v.system.α)
     β = GraphDynamicsInterop.recursive_getdefault(v.system.β)
+    Glu_max = GraphDynamicsInterop.recursive_getdefault(v.Glu_max)
+    τ_Glu = GraphDynamicsInterop.recursive_getdefault(v.τ_Glu)
+    θ = GraphDynamicsInterop.recursive_getdefault(v.θ)
 
-    params = SubsystemParams{AdamNMDAR}(; k_on, k_off, k_r, k_d, k_unblock, k_block, α, β)
+    params = SubsystemParams{AdamNMDAR}(; k_on, k_off, k_r, k_d, k_unblock, k_block, α, β, Glu_max, τ_Glu, θ)
 
     # Extract the default values of states
     C = GraphDynamicsInterop.recursive_getdefault(v.system.C)
@@ -172,18 +174,24 @@ function GraphDynamicsInterop.to_subsystem(v::AdamNMDAR)
     D_AAB = GraphDynamicsInterop.recursive_getdefault(v.system.D_AAB)
     C_AB = GraphDynamicsInterop.recursive_getdefault(v.system.C_AB)
     C_B = GraphDynamicsInterop.recursive_getdefault(v.system.C_B)
-    states = SubsystemStates{AdamNMDAR}(; C, C_A, C_AA, D_AA, O_AA, O_AAB, C_AAB, D_AAB, C_AB, C_B)
+    Glu = GraphDynamicsInterop.recursive_getdefault(v.Glu)
+
+    states = SubsystemStates{AdamNMDAR}(; C, C_A, C_AA, D_AA, O_AA, O_AAB, C_AAB, D_AAB, C_AB, C_B, Glu)
 
     Subsystem(states, params)
 end
 
-GraphDynamicsInterop.initialize_input(s::Subsystem{AdamNMDAR}) = (; Glu = 0.0, V = 0.0)
+GraphDynamicsInterop.initialize_input(s::Subsystem{AdamNMDAR}) = (; jcn = 0.0, V = 0.0)
+
+function heaviside(x)
+    IfElse.ifelse(x > 0, 1.0, 0.0)
+end
 
 function GraphDynamicsInterop.subsystem_differential(s::Subsystem{AdamNMDAR}, inputs, t)
     # Unpack
-    (; Glu, V) = inputs
-    (; C, C_A, C_AA, D_AA, O_AA, O_AAB, C_AAB, D_AAB, C_AB, C_B) = s
-    (; k_on, k_off, k_r, k_d, k_unblock, k_block, α, β) = s
+    (; jcn, V) = inputs
+    (; C, C_A, C_AA, D_AA, O_AA, O_AAB, C_AAB, D_AAB, C_AB, C_B, Glu) = s
+    (; k_on, k_off, k_r, k_d, k_unblock, k_block, α, β, Glu_max, τ_Glu, θ) = s
     return SubsystemStates{AdamNMDAR}(
         #=d/dt=# C = k_off*C_A - 2*k_on*Glu*C,
         #=d/dt=# C_A = 2*k_off*C_AA + 2*k_on*Glu*C - (k_on*Glu + k_off)*C_A,
@@ -194,7 +202,8 @@ function GraphDynamicsInterop.subsystem_differential(s::Subsystem{AdamNMDAR}, in
         #=d/dt=# C_AAB = α*O_AAB + k_on*Glu*C_AB + k_r*D_AAB - (β + 2*k_off + k_d)*C_AAB,
         #=d/dt=# D_AAB = k_d*C_AAB - k_r*D_AAB,
         #=d/dt=# C_AB = 2*k_off*C_AAB + 2*k_on*Glu*C_B - (k_on*Glu + k_off)*C_AB,
-        #=d/dt=# C_B = k_off*C_AB - 2*k_on*Glu*C_B
+        #=d/dt=# C_B = k_off*C_AB - 2*k_on*Glu*C_B,
+        #=d/dt=# Glu = Glu_max*heaviside(jcn - θ) - Glu/τ_Glu
     )
 end
 
