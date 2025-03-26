cb08230700e624ea9fc7f7840d545dcd95aa5814
David Hofmann <david@lcneuro.org>
2025-03-26 19:52:25 +0100

    and another missed patch

--- test/datafitting.jl
+++ test/datafitting.jl
@@ -229,11 +229,11 @@ end
 
     csdsetup = (mar_order = 8, freq = freq, dt = dt);
 
-    (state, setup) = setup_sDCM(data, fullmodel, initcond, csdsetup, priors, hyperpriors, indices, modelparam, "LFP");
+    (state, setup) = setup_spDCM(data, fullmodel, initcond, csdsetup, priors, hyperpriors, indices, modelparam, "LFP");
 
     for iter in 1:128
         state.iter = iter
-        run_sDCM_iteration!(state, setup)
+        run_spDCM_iteration!(state, setup)
         print("iteration: ", iter, " - F:", state.F[end] - state.F[2], " - dF predicted:", state.dF[end], "\n")
         if iter >= 4
             criterion = state.dF[end-3:end] .< setup.tolerance
