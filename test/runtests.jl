58cd0e3e8896ac2db387503f3a15f21352494ac7
Mason Protter <mason.protter@icloud.com>
2025-05-14 13:29:20 +0200

    shift some tests from Advanced to Basics

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,13 +6,13 @@ if GROUP == "All" || GROUP == "Basics"
     @time @safetestset "Utilities" begin include("utils.jl") end
     @time @safetestset "Components Tests" begin include("components.jl") end
     @time @safetestset "Neurograph Tests" begin include("graphs.jl") end
+    @time @safetestset "Learning Tests" begin include("learning.jl") end
+    @time @safetestset "DBS" begin include("dbs.jl") end
 end
 
 if GROUP == "All" || GROUP == "Advanced"
-    @time @safetestset "Learning Tests" begin include("learning.jl") end
     @time @safetestset "Reinforcement Learning Tests" begin include("reinforcement_learning.jl") end
     @time @safetestset "Cort-Cort plasticity Tests" begin include("plasticity.jl") end
-    @time @safetestset "DBS" begin include("dbs.jl") end
 end
 
 
