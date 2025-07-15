2a4d7ca91b1eac6eaf446900c58e2427c61abde3
Mason Protter <mason.protter@icloud.com>
2025-07-15 10:26:12 -0400

    delete accidentally included file

--- src/graph_dynamics_dsl_macro.jl
+++
@@ -1,9 +0,0 @@
-macro neuroblox_model(fdef)
-    if fdef.head != :function
-        error("something informative")
-    end
-    sig, body = fdef.args
-    for ex ∈ body.args
-        
-    end
-end
